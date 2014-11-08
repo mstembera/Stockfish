eef297091bb502e54e436dbea9c383c7bdadc35d
mstembera <MissingEmail@email>
2014-11-07 21:55:32 -0800

    Another version of later king safety to include 3 minors but not RB or RN. bench: 7302735

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -221,7 +221,7 @@ namespace {
     ei.attackedBy[Us][ALL_PIECES] = ei.attackedBy[Us][PAWN] = ei.pi->pawn_attacks(Us);
 
     // Init king safety tables only if we are going to use them
-    if (pos.non_pawn_material(Us) >= QueenValueMg)
+    if (pos.non_pawn_material(Us) >= 3 * KnightValueMg)
     {
         ei.kingRing[Them] = b | shift_bb<Down>(b);
         b &= ei.attackedBy[Us][PAWN];
