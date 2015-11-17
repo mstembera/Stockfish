74f99c5c08c2bb52e73dac09e40217f40ac482c1
mstembera <MissingEmail@email>
2015-11-16 17:09:01 -0800

    Remove windows specific stuff and always use spinlocks implemented using standard STL.

--- src/thread_win32.h
+++ src/thread_win32.h
@@ -17,9 +17,6 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef THREAD_WIN32_H_INCLUDED
-#define THREAD_WIN32_H_INCLUDED
-
 /// STL thread library used by mingw and gcc when cross compiling for Windows
 /// relies on libwinpthread. Currently libwinpthread implements mutexes directly
 /// on top of Windows semaphores. Semaphores, being kernel objects, require kernel
@@ -31,39 +28,21 @@
 /// speed performance of SRW locks.
 
 #include <condition_variable>
-#include <mutex>
-
-#if defined(_WIN32) && !defined(_MSC_VER)
-
-#ifndef NOMINMAX
-#  define NOMINMAX // Disable macros min() and max()
-#endif
-
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#undef WIN32_LEAN_AND_MEAN
-#undef NOMINMAX
 
 /// Mutex and ConditionVariable struct are wrappers of the low level locking
 /// machinery and are modeled after the corresponding C++11 classes.
 
-struct Mutex {
-  Mutex() { InitializeCriticalSection(&cs); }
- ~Mutex() { DeleteCriticalSection(&cs); }
-  void lock() { EnterCriticalSection(&cs); }
-  void unlock() { LeaveCriticalSection(&cs); }
+class Mutex {
+public:
+  Mutex() { flag.clear(); }
+  void lock() {
+      for (unsigned i = 0;    flag.test_and_set(std::memory_order_acquire) 
+                           && (i < 10000 || [](){ std::this_thread::yield(); return true; }()); ++i);
+  }
+  void unlock() { flag.clear(std::memory_order_release); }
 
 private:
-  CRITICAL_SECTION cs;
+  std::atomic_flag flag;
 };
 
 typedef std::condition_variable_any ConditionVariable;
-
-#else // Default case: use STL classes
-
-typedef std::mutex Mutex;
-typedef std::condition_variable ConditionVariable;
-
-#endif
-
-#endif // #ifndef THREAD_WIN32_H_INCLUDED
