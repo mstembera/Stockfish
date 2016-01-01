e17b56e70231982508abc19d5564b5def180457d
mstembera <MissingEmail@email>
2016-01-01 15:14:11 -0800

    King safety attempt related to castling. bench: 8710696

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -299,14 +299,19 @@ Score Entry::do_king_safety(const Position& pos, Square ksq) {
   if (relative_rank(Us, ksq) > RANK_4)
       return make_score(0, -16 * minKingPawnDistance);
 
-  Value bonus = shelter_storm<Us>(pos, ksq);
+  Value bonus = shelter_storm<Us>(pos, ksq) + 4;
 
-  // If we can castle use the bonus after the castling if it is bigger
-  if (pos.can_castle(MakeCastling<Us, KING_SIDE>::right))
-      bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_G1)));
+  // If we can castle use the bonus after the castling if it is bigger.
+  if (!pos.did_castle(Us))
+  {
+      if (pos.can_castle(MakeCastling<Us, KING_SIDE>::right))
+          bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_G1)) - 2);
 
-  if (pos.can_castle(MakeCastling<Us, QUEEN_SIDE>::right))
-      bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_C1)));
+      if (pos.can_castle(MakeCastling<Us, QUEEN_SIDE>::right))
+          bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_C1)) - 2);
+  }
+  else // Bonus for having castled.
+      bonus += 10;
 
   return make_score(bonus, -16 * minKingPawnDistance);
 }
