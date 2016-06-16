d347b83f532e875a051e6d59f1b404d0cb427e18
mstembera <MissingEmail@email>
2016-06-15 18:40:46 -0700

    Cap initiative to 1/4 instead of 1/2. bench: 8159359

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -693,8 +693,8 @@ namespace {
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
-    // that the endgame score will never be divided by more than two.
-    int value = ((eg > 0) - (eg < 0)) * std::max(initiative, -abs(eg / 2));
+    // that the endgame score will never be divided by more than four.
+    int value = ((eg > 0) - (eg < 0)) * std::max(initiative, -abs(eg / 4));
 
     return make_score(0, value);
   }
