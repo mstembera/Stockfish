fc9620102c3a81bb39a4e45580548afb8061c107
mstembera <MissingEmail@email>
2017-05-04 19:01:40 -0700

    Take 2 bench: 5961139

--- src/tt.h
+++ src/tt.h
@@ -52,7 +52,8 @@ struct TTEntry {
 
     // Don't overwrite more valuable entries
     if (  (k >> 48) != key16
-        || d / ONE_PLY + (b == BOUND_EXACT) * 4 > depth8 - 4)
+        ||   d / ONE_PLY + (b == BOUND_EXACT) * 4
+           > depth8 + ((genBound8 & 0x3) == BOUND_EXACT) * 4 - 4)
      /* || g != (genBound8 & 0xFC) // Matching non-zero keys are already refreshed by probe() */
     {
         key16     = (uint16_t)(k >> 48);
