198c63c3bec43230de3c0e124345e058a9efd019
mstembera <MissingEmail@email>
2017-10-22 15:06:19 -0700

    Penalize low mobility in initiative. bench: 5286996
    Idea from:
    https://groups.google.com/forum/?fromgroups=#!topic/fishcooking/1hSKT5PdiTQ

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -754,9 +754,11 @@ namespace {
     int kingDistance =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                       - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
     bool bothFlanks = (pos.pieces(PAWN) & QueenSide) && (pos.pieces(PAWN) & KingSide);
+    bool lowMobility = (mobility[WHITE] + mobility[BLACK]) < 0;
 
     // Compute the initiative bonus for the attacking side
-    int initiative = 8 * (pe->pawn_asymmetry() + kingDistance - 17) + 12 * pos.count<PAWN>() + 16 * bothFlanks;
+    int initiative =   8 * (pe->pawn_asymmetry() + kingDistance - 17) 
+                    + 12 * pos.count<PAWN>() + 16 * (bothFlanks - lowMobility);
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
