2338ed6b1cc9b7dd3be7492efff5c2e4fd48fad7
mstembera <MissingEmail@email>
2017-10-23 17:43:39 -0700

    Fixed and modified version. Thanks to @snicolet bench: 5693837

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -754,11 +754,11 @@ namespace {
     int kingDistance =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                       - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
     bool bothFlanks = (pos.pieces(PAWN) & QueenSide) && (pos.pieces(PAWN) & KingSide);
-    bool lowMobility = (mobility[WHITE] + mobility[BLACK]) < 0;
 
     // Compute the initiative bonus for the attacking side
-    int initiative =   8 * (pe->pawn_asymmetry() + kingDistance - 17) 
-                    + 12 * pos.count<PAWN>() + 16 * (bothFlanks - lowMobility);
+    int initiative =  8 * (pe->pawn_asymmetry() + kingDistance) - 145
+                   + 12 * pos.count<PAWN>() + 16 * bothFlanks
+                   + eg_value(mobility[WHITE] + mobility[BLACK]) / 32;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
