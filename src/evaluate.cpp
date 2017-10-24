939c26a75d7e965e08f32ef0897c945490e7b184
mstembera <MissingEmail@email>
2017-10-24 06:05:25 -0700

    Try both high and imbalanced mobility in initiative. bench: 4805254

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -754,11 +754,12 @@ namespace {
     int kingDistance =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                       - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
     bool bothFlanks = (pos.pieces(PAWN) & QueenSide) && (pos.pieces(PAWN) & KingSide);
+    int mobW = eg_value(mobility[WHITE]), mobB = eg_value(mobility[BLACK]);
 
     // Compute the initiative bonus for the attacking side
-    int initiative =  8 * (pe->pawn_asymmetry() + kingDistance) - 145
-                   + 12 * pos.count<PAWN>() + 16 * bothFlanks
-                   + eg_value(mobility[WHITE] + mobility[BLACK]) / 32;
+    int initiative =   8 * (pe->pawn_asymmetry() + kingDistance) - 150
+                    + 12 * pos.count<PAWN>() + 16 * bothFlanks
+                    + (mobW + mobB + abs(mobW - mobB) * 4) / 32;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
