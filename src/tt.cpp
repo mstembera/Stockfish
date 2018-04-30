06c70c8c79816dbfeded791114a69a4c066315d8
mstembera <MissingEmail@email>
2018-04-30 02:39:32 -0700

    Use stl in TT replacement policy. No functional change. bench: 5254862

--- src/tt.cpp
+++ src/tt.cpp
@@ -20,6 +20,7 @@
 
 #include <cstring>   // For std::memset
 #include <iostream>
+#include <algorithm>
 
 #include "bitboard.h"
 #include "tt.h"
@@ -87,17 +88,15 @@ TTEntry* TranspositionTable::probe(const Key key, bool& found) const {
       }
 
   // Find an entry to be replaced according to the replacement strategy
-  TTEntry* replace = tte;
-  for (int i = 1; i < ClusterSize; ++i)
+  int p[ClusterSize];
+  for (int i = 0; i < ClusterSize; ++i)
       // Due to our packed storage format for generation and its cyclic
       // nature we add 259 (256 is the modulus plus 3 to keep the lowest
       // two bound bits from affecting the result) to calculate the entry
       // age correctly even after generation8 overflows into the next cycle.
-      if (  replace->depth8 - ((259 + generation8 - replace->genBound8) & 0xFC) * 2
-          >   tte[i].depth8 - ((259 + generation8 -   tte[i].genBound8) & 0xFC) * 2)
-          replace = &tte[i];
+      p[i] = tte[i].depth8 - ((259 + generation8 - tte[i].genBound8) & 0xFC) * 2;
 
-  return found = false, replace;
+  return found = false, &tte[std::min_element(p, p + ClusterSize) - p];
 }
 
 
