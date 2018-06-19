a2a9b8dc511fc7701c239a1e3d1893c86ed3bf8f
mstembera <MissingEmail@email>
2018-06-19 15:42:48 -0700

    Use mobile pawn count in initiative. bench: 4726078

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -773,7 +773,8 @@ namespace {
     // Compute the initiative bonus for the attacking side
     int complexity =   8 * outflanking
                     +  8 * pe->pawn_asymmetry()
-                    + 12 * pos.count<PAWN>()
+                    +  6 * (pe->mobile_count(WHITE) + pe->mobile_count(BLACK))
+                    + 10 * pos.count<PAWN>()
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
                     -136 ;
