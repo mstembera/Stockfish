5e64e855332db015b402672c196e8edc506e64ca
mstembera <MissingEmail@email>
2018-07-19 13:39:58 -0700

    Tweak history weight in evasion scoring. bench: 4552523

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -125,7 +125,7 @@ void MovePicker::score() {
               m.value =  PieceValue[MG][pos.piece_on(to_sq(m))]
                        - Value(type_of(pos.moved_piece(m)));
           else
-              m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
+              m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)] * 2
                        + (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                        - (1 << 28);
       }
