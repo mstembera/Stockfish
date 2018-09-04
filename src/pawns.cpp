6762cbbc183ed10797311d991b989b8505998088
mstembera <MissingEmail@email>
2018-09-04 00:25:35 -0700

    Tuned values. bench: 4396106

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -35,9 +35,13 @@ namespace {
   constexpr Score Backward = S( 9, 24);
   constexpr Score Doubled  = S(11, 56);
   constexpr Score Isolated = S( 5, 15);
+ constexpr Score Islands[5] = { S(2, -1), S(-1, -1), S(4, 0), S(-2, 0), S(-4, 4) };
 
   // Connected pawn bonus by opposed, phalanx, #support and rank
   Score Connected[2][2][3][RANK_NB];
+
+  // Number of pawn islands indexed by open files
+  uint8_t islandCount[256];
 
   // Strength of pawn shelter for our king by [distance from edge][rank].
   // RANK_1 = 0 is used for files where we have no pawn, or pawn is behind our king.
@@ -145,6 +149,9 @@ namespace {
             score -= Doubled;
     }
 
+    int islandCnt = islandCount[e->semiopenFiles[Us]];
+    score += Islands[islandCnt];
+
     return score;
   }
 
@@ -170,6 +177,21 @@ void init() {
 
       Connected[opposed][phalanx][support][r] = make_score(v, v * (r - 2) / 4);
   }
+
+  for (int openFiles = 0; openFiles < 256; ++openFiles)
+  {
+      islandCount[openFiles] = 0;
+      Bitboard pawnFiles = 0xFF ^ openFiles;
+      int f = INT_MIN;
+
+      while (pawnFiles)
+      {
+          int fNext = pop_lsb(&pawnFiles);
+          islandCount[openFiles] += fNext > f + 1;
+          f = fNext;
+      }
+  }
+
 }
 
 
