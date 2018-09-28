36d12035fd92915a36a41bc99ac180a453b459b5
mstembera <MissingEmail@email>
2018-09-28 15:02:06 -0700

    Take 2 bench: 4602410

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -35,7 +35,7 @@ namespace {
   constexpr Score Backward = S( 9, 24);
   constexpr Score Doubled  = S(11, 56);
   constexpr Score Isolated = S( 5, 15);
-  constexpr Score MostAdvanced = S(2, 0);
+  constexpr Score MostAdvanced = S(0, 2);
 
   // Connected pawn bonus by opposed, phalanx, #support and rank
   Score Connected[2][2][3][RANK_NB];
