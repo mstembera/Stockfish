644fde743677559df58ec08757eb2138949faf86
mstembera <MissingEmail@email>
2018-09-28 15:11:59 -0700

    Trapped rook tweak. bench: 4253433

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -380,7 +380,7 @@ namespace {
                 score += RookOnFile[bool(pe->semiopen_file(Them, file_of(s)))];
 
             // Penalty when trapped by the king, even more if the king cannot castle
-            else if (mob <= 3)
+            else if (mob <= 3 && relative_rank(Us, s) == relative_rank(Us, pos.square<KING>(Us)))
             {
                 File kf = file_of(pos.square<KING>(Us));
                 if ((kf < FILE_E) == (file_of(s) < kf))
