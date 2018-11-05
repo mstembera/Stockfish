226e1ec5e634b5fce4046ed09f6999bc2a921c74
mstembera <MissingEmail@email>
2018-11-05 13:33:09 -0800

    Take 4 bench: 3346127

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -33,7 +33,7 @@ namespace {
 
   // Pawn penalties
   constexpr Score Backward = S( 9, 24);
-  constexpr Score Doubled  = S( 7, 38);
+  constexpr Score Doubled  = S( 8, 42);
   constexpr Score Isolated = S( 5, 15);
 
   // Connected pawn bonus by opposed, phalanx, #support and rank
