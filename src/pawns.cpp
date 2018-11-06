49d7b76a5e508e69f4b00c9dc7d8708fbcb529ef
mstembera <MissingEmail@email>
2018-11-05 18:45:33 -0800

    Take 5  Only consider the outer 2 files. bench: 3823311

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -148,13 +148,13 @@ namespace {
     // Bonus for ability to capture towards the center
     constexpr Direction inwardQ  = (Us == WHITE) ? NORTH_EAST : SOUTH_EAST;
     constexpr Direction inwardK  = (Us == WHITE) ? NORTH_WEST : SOUTH_WEST;
-    constexpr Bitboard queenSide = FileABB | FileBBB | FileCBB;
-    constexpr Bitboard kingSide  = FileFBB | FileGBB | FileHBB;
+    constexpr Bitboard queenSide = FileABB | FileBBB;
+    constexpr Bitboard kingSide  = FileGBB | FileHBB;
 
     Bitboard inwardAttacks = theirPawns
         & (shift<inwardQ>(ourPawns & queenSide) | shift<inwardK>(ourPawns & kingSide));
     
-    int inwardBonus = popcount(inwardAttacks) * 3;
+    int inwardBonus = popcount(inwardAttacks) * 4;
     score += make_score(inwardBonus, 0);
 
     return score;
