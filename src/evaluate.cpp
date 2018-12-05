fb6a3cffb5310cc3283a1f7e43c1512676f28740
mstembera <MissingEmail@email>
2018-12-05 01:21:32 -0800

    Take 2 bench: 3381356

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -757,7 +757,7 @@ namespace {
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
-                    -  4 * ramCnt
+                    -  6 * ramCnt
                     - 116;
 
     // Now apply the bonus: note that we find the attacking side by extracting
