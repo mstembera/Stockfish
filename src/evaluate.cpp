fae4187f01ba84c4b5db5bb5379e36af2364f592
mstembera <MissingEmail@email>
2018-12-10 13:32:17 -0800

    Allow queen Xrays through our bishops. bench: 3478399

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -300,8 +300,8 @@ namespace {
         // Find attacked squares, including some x-ray attacks for bishops, rooks, and queens
         b = Pt == BISHOP ? attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(QUEEN))
           : Pt ==   ROOK ? attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(QUEEN) ^ pos.pieces(Us, ROOK))
-          : Pt ==  QUEEN ?   attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(Us, ROOK))
-                           | attacks_bb<BISHOP>(s, pos.pieces())
+          : Pt ==  QUEEN ?   attacks_bb<  ROOK>(s, pos.pieces())
+                           | attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(Us, BISHOP))
                          : pos.attacks_from<Pt>(s);
 
         if (pos.blockers_for_king(Us) & s)
