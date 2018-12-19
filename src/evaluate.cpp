7aa9b3f9e3d199cdb3c2ee069d2c3d7e0f91d47d
mstembera <MissingEmail@email>
2018-12-18 22:16:54 -0800

    Outpost tweak bench: 3308751

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -328,7 +328,7 @@ namespace {
             if (bb & s)
                 score += Outpost[Pt == BISHOP][bool(attackedBy[Us][PAWN] & s)] * 2;
 
-            else if (bb &= b & ~pos.pieces(Us))
+            else if (bb &= b & ~pos.pieces(Us) & ~attackedBy[Them][ALL_PIECES])
                 score += Outpost[Pt == BISHOP][bool(attackedBy[Us][PAWN] & bb)];
 
             // Knight and Bishop bonus for being right behind a pawn
