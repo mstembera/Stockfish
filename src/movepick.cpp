e01ae3923799f54288fc2e71b6c8b6f1c79fe87d
mstembera <MissingEmail@email>
2019-01-03 14:37:13 -0800

    A variant of this idea by @xoroshiro http://tests.stockfishchess.org/tests/view/5c2e47d70ebc596a450bc6da bench: 3088932

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -203,7 +203,7 @@ top:
       endMoves = generate<QUIETS>(pos, cur);
 
       score<QUIETS>();
-      partial_insertion_sort(cur, endMoves, -4000 * depth / ONE_PLY);
+      partial_insertion_sort(cur, endMoves, -150 * int(endMoves - cur) * depth / ONE_PLY);
       ++stage;
       /* fallthrough */
 
