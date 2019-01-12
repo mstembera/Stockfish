d81df239c5561cc1009135dc906678721a005a08
mstembera <MissingEmail@email>
2019-01-11 16:37:39 -0800

    Original patch by ElbertoOne http://tests.stockfishchess.org/tests/view/5c35f4bb0ebc596a450c682d bench: 4024176

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -171,6 +171,7 @@ namespace {
   constexpr Score TrappedRook        = S( 96,  4);
   constexpr Score WeakQueen          = S( 49, 15);
   constexpr Score WeakUnopposedPawn  = S( 12, 23);
+  constexpr Score HinderMinor        = S( 10, 15);
 
 #undef S
 
@@ -477,6 +478,9 @@ namespace {
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 0)
         score -= make_score(kingDanger * kingDanger / 4096, kingDanger / 16);
+    // Penalty if our king hinders the mobility of a minor.
+    else if ((attackedBy[Us][BISHOP] | attackedBy[Us][KNIGHT]) & ksq)
+        score -= HinderMinor;
 
     // Penalty when our king is on a pawnless flank
     if (!(pos.pieces(PAWN) & kingFlank))
