6b8503a82571ae2195b0b364f68ee47bb8fa71c6
mstembera <MissingEmail@email>
2019-01-16 12:23:06 -0800

    Optimization in MovePicker::select() bench: 3977000

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -136,9 +136,13 @@ Move MovePicker::select(Pred filter) {
   while (cur < endMoves)
   {
       if (T == Best)
-          std::swap(*cur, *std::max_element(cur, endMoves));
-
-      move = *cur++;
+      {
+          auto maxMove = std::max_element(cur, endMoves);
+          move = *maxMove;
+          *maxMove = *cur++;
+      }
+      else
+        move = *cur++;
 
       if (move != ttMove && filter())
           return move;
