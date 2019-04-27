d030bd41f429eb12db736f205fca8052032ba52d
mstembera <MissingEmail@email>
2019-04-27 02:50:20 -0700

    Tweak king danger bench: 3336203

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -466,8 +466,8 @@ namespace {
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  +  69 * kingAttacksCount[Them]
                  + 185 * popcount(kingRing[Us] & weak)
-                 - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
-                 -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
+                 - 100 * bool(attackedBy[Us][KNIGHT] & (attackedBy[Us][KING] | ksq))
+                 -  35 * bool(attackedBy[Us][BISHOP] & (attackedBy[Us][KING] | ksq))
                  + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
