4df35fdf34dfee9e7499c44ce30a527bdf36bb21
mstembera <MissingEmail@email>
2019-05-29 18:26:16 -0700

    Tweak outflanking bench: 3889199

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -736,8 +736,9 @@ namespace {
   template<Tracing T>
   Score Evaluation<T>::initiative(Value eg) const {
 
-    int outflanking =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
-                     - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
+    int outflanking = rank_of(pos.square<KING>(WHITE)) - rank_of(pos.square<KING>(BLACK));
+    if (outflanking <= 0)
+        outflanking += distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
 
     bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
                             && (pos.pieces(PAWN) & KingSide);
