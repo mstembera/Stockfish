303052b2ff03e8db02e45dde0901e3dd863403a1
mstembera <MissingEmail@email>
2019-06-16 17:18:39 -0700

    Smaller still bench: 3911881

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -174,7 +174,7 @@ top:
 
   case GOOD_CAPTURE:
       if (select<Best>([&](){
-                       return pos.see_ge(*cur, Value(-25 - 55 * cur->value / 1024)) ?
+                       return pos.see_ge(*cur, Value(-10 - 55 * cur->value / 1024)) ?
                               // Move losing capture to endBadCaptures to be tried later
                               true : (*endBadCaptures++ = *cur, false); }))
           return *(cur - 1);
