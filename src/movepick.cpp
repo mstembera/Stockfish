9353f2598d0400d28ba93d80edabe89117012b1f
mstembera <MissingEmail@email>
2019-06-24 19:54:22 -0700

    Score non normal moves higher bench: 3821695

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -108,7 +108,8 @@ void MovePicker::score() {
   for (auto& m : *this)
       if (Type == CAPTURES)
           m.value =  PieceValue[MG][pos.piece_on(to_sq(m))]
-                   + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))] / 8;
+                   + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))] / 8
+                   + (type_of(m) != NORMAL) * 8;
 
       else if (Type == QUIETS)
           m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
