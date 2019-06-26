d0a6ebb160ae1f6a63d90b052822107d46e01a95
mstembera <MissingEmail@email>
2019-06-26 14:13:42 -0700

    Less king danger if we have pawns in king ring bench: 3752427

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -460,6 +460,7 @@ namespace {
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  +  69 * kingAttacksCount[Them]
                  + 185 * popcount(kingRing[Us] & weak)
+                 -  25 * popcount(kingRing[Us] & pos.pieces(Us, PAWN))
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
                  + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
@@ -467,7 +468,7 @@ namespace {
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
-                 -   7;
+                 +  20;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
