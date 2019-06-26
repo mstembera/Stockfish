8795b7e0a0ca6d3f420d56e732cd36e9bd7d23c0
mstembera <MissingEmail@email>
2019-06-25 19:25:30 -0700

    Higher scores for pawn captures toward the center. bench: 3626567

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -108,7 +108,10 @@ void MovePicker::score() {
   for (auto& m : *this)
       if (Type == CAPTURES)
           m.value =  PieceValue[MG][pos.piece_on(to_sq(m))]
-                   + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))] / 8;
+                   + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))] / 8
+                   + (   type_of(pos.moved_piece(m)) == PAWN
+                      && (   (file_of(to_sq(m)) < FILE_E && file_of(to_sq(m)) > file_of(from_sq(m)))  
+                          || (file_of(to_sq(m)) > FILE_D && file_of(to_sq(m)) < file_of(from_sq(m))))) * 16;
 
       else if (Type == QUIETS)
           m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
