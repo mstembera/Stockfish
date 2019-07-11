f2b8b31ac992bfb0a9ccb768d3ea0898a7177d27
mstembera <MissingEmail@email>
2019-07-11 10:47:19 -0700

    Use side to move in initiative bench: 3243038

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -734,13 +734,16 @@ namespace {
     bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
                             && (pos.pieces(PAWN) & KingSide);
 
+    Color strongSide = eg >= 0 ? WHITE : BLACK;
+
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
+                    + 20 * (strongSide == pos.side_to_move())
                     + 49 * !pos.non_pawn_material()
-                    -103 ;
+                    -113;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
