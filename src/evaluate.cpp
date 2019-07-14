b6342c1ed5b80a3bfdef1dae0a5087fd6393fe5d
mstembera <MissingEmail@email>
2019-07-13 23:12:59 -0700

    Use phase and tempo for lazy threshold bench: 3223426

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -801,9 +801,11 @@ namespace {
     score += pe->pawn_score(WHITE) - pe->pawn_score(BLACK);
 
     // Early exit if score is high
-    Value v = (mg_value(score) + eg_value(score)) / 2;
+    Value v =  mg_value(score) * int(me->game_phase())
+             + eg_value(score) * int(PHASE_MIDGAME - me->game_phase());
+    v /= PHASE_MIDGAME;
     if (abs(v) > LazyThreshold + pos.non_pawn_material() / 64)
-       return pos.side_to_move() == WHITE ? v : -v;
+       return (pos.side_to_move() == WHITE ? v : -v) + Eval::Tempo;
 
     // Main evaluation begins here
 
