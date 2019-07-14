b2d2f2368dd878981380aa9fc9e9f2c733b1abaf
mstembera <MissingEmail@email>
2019-07-14 13:25:50 -0700

    Initiative bonus for pawns on 7th bench: 3394171

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -731,13 +731,17 @@ namespace {
     bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
                             && (pos.pieces(PAWN) & KingSide);
 
+    int advanced = popcount(  (pos.pieces(WHITE, PAWN) & Rank7BB)
+                            | (pos.pieces(BLACK, PAWN) & Rank2BB));
+
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
+                    +  9 * advanced
                     + 49 * !pos.non_pawn_material()
-                    -103 ;
+                    -106 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
