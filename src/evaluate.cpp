c7103158439ec31db1a266057c50138db586b158
mstembera <MissingEmail@email>
2019-07-23 16:11:04 -0700

    Take 2 bench: 3671274

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -356,7 +356,7 @@ namespace {
                 File kf = file_of(pos.square<KING>(Us));
                 if ((kf < FILE_E) == (file_of(s) < kf))
                     score -= TrappedRook * (1 + (   !pos.castling_rights(Us)
-                                                 && rank_of(s) == rank_of(pos.square<KING>(Us))));
+                                                 && relative_rank(Us, pos.square<KING>(Us)) == RANK_1));
             }
         }
 
