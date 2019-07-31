2204b253afdcec418fdd32e964d28355a91460ca
mstembera <MissingEmail@email>
2019-07-31 14:27:22 -0700

    Tweak trapped rook based on mobility bench: 4005454

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -145,7 +145,7 @@ namespace {
   constexpr Score ThreatByPawnPush   = S( 48, 39);
   constexpr Score ThreatByRank       = S( 13,  0);
   constexpr Score ThreatBySafePawn   = S(173, 94);
-  constexpr Score TrappedRook        = S( 47,  4);
+  constexpr Score TrappedRook        = S( 12,  1);
   constexpr Score WeakQueen          = S( 49, 15);
 
 #undef S
@@ -355,7 +355,7 @@ namespace {
             {
                 File kf = file_of(pos.square<KING>(Us));
                 if ((kf < FILE_E) == (file_of(s) < kf))
-                    score -= TrappedRook * (1 + !pos.castling_rights(Us));
+                    score -= TrappedRook * ((1 + !pos.castling_rights(Us)) * 4 + 2 - mob);
             }
         }
 
