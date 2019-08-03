2d4d3de9d575bc892d275ab756e4806fd506c127
mstembera <MissingEmail@email>
2019-08-03 04:31:48 -0700

    Take 2 bench: 4036762

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -142,7 +142,7 @@ namespace {
             score -= Backward + WeakUnopposed * int(!opposed);
 
         if (doubled)
-            score -= Doubled * (11 - 8 * bool(support));
+            score -= Doubled * (11 - 9 * bool(support));
 
     }
 
