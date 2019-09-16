4e6664234643551bd51984dc1078dcfe0015f69d
mstembera <MissingEmail@email>
2019-09-16 16:04:11 -0700

    Other way bench: 3866799

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -741,7 +741,7 @@ namespace {
                     - 36 * almostUnwinnable
                     -103 ;
 
-    complexity = complexity * 31 / 32;
+    complexity = complexity * 33 / 32;
 
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
