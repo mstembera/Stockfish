b402d9e8cd4d3ee225ae2d57320ec66a17e4f256
mstembera <MissingEmail@email>
2019-09-26 23:09:30 -0700

    Skip expensive imbalance() calculation when we can. No functional change. bench: 3618154

--- src/material.cpp
+++ src/material.cpp
@@ -203,16 +203,21 @@ Entry* probe(const Position& pos) {
       e->factor[BLACK] = uint8_t(npm_b <  RookValueMg   ? SCALE_FACTOR_DRAW :
                                  npm_w <= BishopValueMg ? 4 : 14);
 
-  // Evaluate the material imbalance. We use PIECE_TYPE_NONE as a place holder
-  // for the bishop pair "extended piece", which allows us to be more flexible
-  // in defining bishop pair bonuses.
-  const int pieceCount[COLOR_NB][PIECE_TYPE_NB] = {
-  { pos.count<BISHOP>(WHITE) > 1, pos.count<PAWN>(WHITE), pos.count<KNIGHT>(WHITE),
-    pos.count<BISHOP>(WHITE)    , pos.count<ROOK>(WHITE), pos.count<QUEEN >(WHITE) },
-  { pos.count<BISHOP>(BLACK) > 1, pos.count<PAWN>(BLACK), pos.count<KNIGHT>(BLACK),
-    pos.count<BISHOP>(BLACK)    , pos.count<ROOK>(BLACK), pos.count<QUEEN >(BLACK) } };
+  if (   pos.non_pawn_material(WHITE) != pos.non_pawn_material(BLACK)
+      || pos.count<PAWN>(WHITE) != pos.count<PAWN>(BLACK))
+  {
+      // Evaluate the material imbalance. We use PIECE_TYPE_NONE as a place holder
+      // for the bishop pair "extended piece", which allows us to be more flexible
+      // in defining bishop pair bonuses.
+      const int pieceCount[COLOR_NB][PIECE_TYPE_NB] = {
+      { pos.count<BISHOP>(WHITE) > 1, pos.count<PAWN>(WHITE), pos.count<KNIGHT>(WHITE),
+        pos.count<BISHOP>(WHITE)    , pos.count<ROOK>(WHITE), pos.count<QUEEN >(WHITE) },
+      { pos.count<BISHOP>(BLACK) > 1, pos.count<PAWN>(BLACK), pos.count<KNIGHT>(BLACK),
+        pos.count<BISHOP>(BLACK)    , pos.count<ROOK>(BLACK), pos.count<QUEEN >(BLACK) } };
 
-  e->value = int16_t((imbalance<WHITE>(pieceCount) - imbalance<BLACK>(pieceCount)) / 16);
+      e->value = int16_t((imbalance<WHITE>(pieceCount) - imbalance<BLACK>(pieceCount)) / 16);
+  }
+
   return e;
 }
 
