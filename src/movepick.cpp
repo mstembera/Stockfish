552242145978a614a4f9ac3fc1420628edb94ae8
mstembera <MissingEmail@email>
2019-10-28 04:08:46 -0700

    Prioritize pawn quiets that attack enemy pieces. bench: 4847372

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -111,12 +111,21 @@ void MovePicker::score() {
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if (Type == QUIETS)
+      {
           m.value =      (*mainHistory)[pos.side_to_move()][from_to(m)]
                    + 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                    + 2 * (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + 2 * (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)];
 
+          if (type_of(pos.moved_piece(m)) == PAWN)
+          {
+              Bitboard b = pos.attacks_from<PAWN>(to_sq(m), pos.side_to_move());
+
+              if (b & (pos.pieces(~pos.side_to_move()) ^ pos.pieces(~pos.side_to_move(), PAWN)))
+                  m.value += 150;
+          }
+      }
       else // Type == EVASIONS
       {
           if (pos.capture(m))
