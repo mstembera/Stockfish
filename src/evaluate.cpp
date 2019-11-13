9920dddd4ef12b85dabe424fd4aecb83879a712f
mstembera <MissingEmail@email>
2019-11-13 12:26:47 -0800

    Use the new OutpostRank[] array for reachable outposts as well. bench: 4805642

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -140,7 +140,6 @@ namespace {
   constexpr Score KnightOnQueen      = S( 16, 12);
   constexpr Score LongDiagonalBishop = S( 45,  0);
   constexpr Score MinorBehindPawn    = S( 18,  3);
-  constexpr Score Outpost            = S( 32, 10);
   constexpr Score PassedFile         = S( 11,  8);
   constexpr Score PawnlessFlank      = S( 17, 95);
   constexpr Score RestrictedPiece    = S(  7,  7);
@@ -300,7 +299,7 @@ namespace {
                 score += OutpostRank[relative_rank(Us, s)] * (Pt == KNIGHT ? 2 : 1);
 
             else if (Pt == KNIGHT && bb & b & ~pos.pieces(Us))
-                score += Outpost;
+                score += OutpostRank[relative_rank(Us, frontmost_sq(Us, bb & b & ~pos.pieces(Us)))];
 
             // Knight and Bishop bonus for being right behind a pawn
             if (shift<Down>(pos.pieces(PAWN)) & s)
