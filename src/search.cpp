632f5fb6b29274b1154b2215b327b91e5941f740
mstembera <MissingEmail@email>
2019-12-12 13:15:20 -0800

    Take 3 bench: 5391194

--- src/search.cpp
+++ src/search.cpp
@@ -812,8 +812,11 @@ namespace {
         &&  eval <= alpha - RazorMargin)
         return qsearch<NT>(pos, ss, alpha, beta);
 
-    improving =  (ss-2)->staticEval == VALUE_NONE ? (ss->staticEval >= -(ss-1)->staticEval + 2 * Eval::Tempo
-              || (ss-1)->staticEval == VALUE_NONE) : ss->staticEval >=  (ss-2)->staticEval;
+    improving =   (ss-2)->staticEval != VALUE_NONE
+                ? ss->staticEval >= (ss-2)->staticEval
+                : (ss-4)->staticEval != VALUE_NONE
+                ? ss->staticEval >= (ss-4)->staticEval
+                : (ss->staticEval >= (ss-6)->staticEval || (ss-6)->staticEval == VALUE_NONE);
 
     // Step 8. Futility pruning: child node (~30 Elo)
     if (   !PvNode
