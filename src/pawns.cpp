7e5fdf7270de25f2dd51699a8664b5cd794b5e8a
mstembera <MissingEmail@email>
2020-01-04 13:50:28 -0800

    Less eg bench: 5084395

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -36,7 +36,7 @@ namespace {
   constexpr Score BlockedStorm  = S(82, 82);
   constexpr Score Doubled       = S(11, 56);
   constexpr Score Isolated      = S( 5, 15);
-  constexpr Score Supporting    = S( 3,  9);
+  constexpr Score Supporting    = S( 3,  5);
   constexpr Score WeakLever     = S( 0, 56);
   constexpr Score WeakUnopposed = S(13, 27);
 
@@ -146,12 +146,11 @@ namespace {
 
                 score += make_score(v, v * (r - 2) / 4);
             }
-
             else if (backward)
                 score -=  Backward
                         + WeakUnopposed * !opposed;
             
-            else if (supporting)
+            if (supporting)
                 score += Supporting;
         }
 
