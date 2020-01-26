fd2e4ade06461407d7fd379394879ca3c753a948
mstembera <MissingEmail@email>
2020-01-25 19:19:21 -0800

    Tweak restricted. bench: 4794207

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -524,7 +524,7 @@ namespace {
     b =   attackedBy[Them][ALL_PIECES]
        & ~stronglyProtected
        &  attackedBy[Us][ALL_PIECES];
-    score += RestrictedPiece * (popcount(b) + popcount(b & pos.pieces()));
+    score += RestrictedPiece * (popcount(b) + popcount(b & pos.pieces(Us)));
 
     // Protected or unattacked squares
     safe = ~attackedBy[Them][ALL_PIECES] | attackedBy[Us][ALL_PIECES];
