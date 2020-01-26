303dc3191609e78021e36f693569ccfae2743ac1
mstembera <MissingEmail@email>
2020-01-25 16:00:32 -0800

    Adjust doubled penalty when isolated. bench: 4866684

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -34,8 +34,8 @@ namespace {
   // Pawn penalties
   constexpr Score Backward      = S( 9, 24);
   constexpr Score BlockedStorm  = S(82, 82);
-  constexpr Score Doubled       = S(11, 56);
-  constexpr Score Isolated      = S( 1,  3);
+  constexpr Score Doubled       = S( 1,  5);
+  constexpr Score Isolated      = S( 5, 15);
   constexpr Score WeakLever     = S( 0, 56);
   constexpr Score WeakUnopposed = S(13, 27);
 
@@ -139,7 +139,7 @@ namespace {
         }
 
         else if (!neighbours)
-            score -=   Isolated * (4 + 2 * doubled)
+            score -=   Isolated
                      + WeakUnopposed * !opposed;
 
         else if (backward)
@@ -147,7 +147,7 @@ namespace {
                      + WeakUnopposed * !opposed;
 
         if (!support)
-            score -=   Doubled * doubled
+            score -=   Doubled * (doubled * 13 - (bool)neighbours * 4)
                      + WeakLever * more_than_one(lever);
     }
 
