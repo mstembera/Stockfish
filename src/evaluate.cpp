899571fba2d4709f2deccee7777ca2684320000a
mstembera <MissingEmail@email>
2020-02-12 13:01:24 -0800

    Combo of http://tests.stockfishchess.org/tests/view/5e3b0026e70d848499f63a10 http://tests.stockfishchess.org/tests/view/5e39e6dee70d848499f639c6 bench: 4619978

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -707,15 +707,15 @@ namespace {
                            &&  outflanking < 0
                            && !pawnsOnBothFlanks;
 
-    bool infiltration = rank_of(pos.square<KING>(WHITE)) > RANK_4
-                     || rank_of(pos.square<KING>(BLACK)) < RANK_5;
+    bool infiltration = rank_of(pos.square<KING>(WHITE)) > RANK_5
+                     || rank_of(pos.square<KING>(BLACK)) < RANK_4;
 
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 21 * pawnsOnBothFlanks
-                    + 24 * infiltration
+                    + 28 * infiltration
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
                     -110 ;
