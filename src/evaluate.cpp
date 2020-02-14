741d5ca9ec884e9804eb9d67284361bba88f5736
mstembera <MissingEmail@email>
2020-02-13 19:33:12 -0800

    Use 50 move rule in initiative bench: 5219351

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -718,7 +718,8 @@ namespace {
                     + 24 * infiltration
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
-                    -110 ;
+                    -      pos.rule50_count() / 4
+                    -105 ;
 
     Value mg = mg_value(score);
     Value eg = eg_value(score);
