7f1af53cff52cc8e490f718d4bd7d70f258a4e31
mstembera <MissingEmail@email>
2020-03-10 13:36:52 -0700

    Take 2 bench: 5246621

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -451,7 +451,7 @@ namespace {
                  +   3 * kingFlankAttack * kingFlankAttack / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  - 873 * !pos.count<QUEEN>(Them)
-                 - 120 * bool(attackedBy[Us][KNIGHT] & shift<pawn_push(Us)>(attackedBy[Us][KING]))
+                 - 128 * bool(attackedBy[Us][KNIGHT] & shift<pawn_push(Us)>(attackedBy[Us][KING]))
                  -   6 * mg_value(score) / 8
                  -   4 * kingFlankDefense
                  +  37;
