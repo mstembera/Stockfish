87b860d39bb801794ecb9182a91a3ee99c5a75f1
mstembera <MissingEmail@email>
2020-03-10 13:34:01 -0700

    Variant of a patch by @Vizvedenec bench: 5266614

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -451,7 +451,7 @@ namespace {
                  +   3 * kingFlankAttack * kingFlankAttack / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  - 873 * !pos.count<QUEEN>(Them)
-                 - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
+                 - 100 * bool(attackedBy[Us][KNIGHT] & shift<pawn_push(Us)>(attackedBy[Us][KING]))
                  -   6 * mg_value(score) / 8
                  -   4 * kingFlankDefense
                  +  37;
