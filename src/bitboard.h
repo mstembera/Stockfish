6417c8d314a3961d9f1e31d8a137bdadb5cc029e
mstembera <MissingEmail@email>
2020-05-24 03:52:12 -0700

    Take2 bench: 4317338

--- src/bitboard.h
+++ src/bitboard.h
@@ -66,14 +66,14 @@ constexpr Bitboard KingSide    = FileEBB | FileFBB | FileGBB | FileHBB;
 constexpr Bitboard Center      = (FileDBB | FileEBB) & (Rank4BB | Rank5BB);
 
 constexpr Bitboard KingFlank[FILE_NB] = {
-  FileABB | FileBBB | FileCBB, 
+  FileABB | FileBBB, 
   FileABB | FileBBB | FileCBB,
   FileBBB | FileCBB | FileDBB,
   FileCBB | FileDBB | FileEBB,
   FileDBB | FileEBB | FileFBB,
   FileEBB | FileFBB | FileGBB,
   FileFBB | FileGBB | FileHBB,
-  FileFBB | FileGBB | FileHBB
+  FileGBB | FileHBB
 };
 
 extern uint8_t PopCnt16[1 << 16];
