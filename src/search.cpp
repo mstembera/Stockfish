9f19f6a2cc79695c03d1335812c0ad52cf7537f7
mstembera <MissingEmail@email>
2020-06-01 13:49:52 -0700

    Idea from patches by @UnaiCorzo https://tests.stockfishchess.org/tests/view/5ed4dc82f29b40b0fc95a774 bench: 4601148

--- src/search.cpp
+++ src/search.cpp
@@ -842,7 +842,7 @@ namespace {
 
     // Step 8. Futility pruning: child node (~50 Elo)
     if (   !PvNode
-        &&  depth < 6
+        &&  depth < 7
         &&  eval - futility_margin(depth, improving) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;
