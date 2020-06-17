2bacc5a8b8e65fff5b6a12c5bc6006977a05a97c
mstembera <MissingEmail@email>
2020-06-16 19:32:11 -0700

    Tweak rook on queen file bench: 4639603

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -351,7 +351,7 @@ namespace {
         if (Pt == ROOK)
         {
             // Bonus for rook on the same file as a queen
-            if (file_bb(s) & pos.pieces(QUEEN))
+            if (file_bb(s) & pos.pieces(Them, QUEEN))
                 score += RookOnQueenFile;
 
             // Bonus for rook on an open or semi-open file
