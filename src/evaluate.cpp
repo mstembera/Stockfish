65cf94a66315027bae5cfb89af4177073e1b6c6a
mstembera <MissingEmail@email>
2020-08-24 02:19:11 -0700

    Take 2 bench: 3859808

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -946,7 +946,7 @@ Value Eval::evaluate(const Position& pos) {
                       : NNUE::evaluate(pos) * 5 / 4 + Tempo;
 
   if (classical && Eval::useNNUE && abs(v) * 16 < NNUEThreshold2 * (16 + pos.rule50_count()))
-      v = (v + (NNUE::evaluate(pos) * 5 / 4 + Tempo) * 15) / 16;
+      v = (v + (NNUE::evaluate(pos) * 5 / 4 + Tempo) * 7) / 8;
 
   // Damp down the evaluation linearly when shuffling
   v = v * (100 - pos.rule50_count()) / 100;
