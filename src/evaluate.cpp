375e22e8187224154e1546f4d933543ad2a4e53e
mstembera <MissingEmail@email>
2020-09-11 14:01:04 -0700

    Try to avoid drawish scores bench: 3827856

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1022,13 +1022,22 @@ Value Eval::evaluate(const Position& pos) {
   bool classical = !Eval::useNNUE
                 ||  useClassical
                 || (abs(eg_value(pos.psq_score())) > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB));
-  Value v = classical ? Evaluation<NO_TRACE>(pos).value()
-                      : NNUE::evaluate(pos) * 5 / 4 + Tempo;
+  Value v;
+  if (classical)
+      v = Evaluation<NO_TRACE>(pos).value();
+  else
+  {
+      v = NNUE::evaluate(pos) * 5 / 4;
+      v = abs(v) < 10 ? ((v + Tempo) * 7 + Evaluation<NO_TRACE>(pos).value()) / 8 : v + Tempo;
+  }
 
   if (   useClassical 
       && Eval::useNNUE 
       && abs(v) * 16 < NNUEThreshold2 * (16 + pos.rule50_count()))
-      v = NNUE::evaluate(pos) * 5 / 4 + Tempo;
+  {
+      Value vnn = NNUE::evaluate(pos) * 5 / 4;
+      v = abs(vnn) < 10 ? ((vnn + Tempo) * 15 + v) / 16 : vnn + Tempo;
+  }
 
   // Damp down the evaluation linearly when shuffling
   v = v * (100 - pos.rule50_count()) / 100;
