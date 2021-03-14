addaed704f4c8b732695cb8814ca461e9dec3202
mstembera <MissingEmail@email>
2021-03-13 17:48:53 -0800

    Retry of https://tests.stockfishchess.org/tests/view/5f43a5955089a564a10d84ed with latest evals and 0 contempt. bench: 4715738

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1119,12 +1119,13 @@ Value Eval::evaluate(const Position& pos) {
       // If the classical eval is small and imbalance large, use NNUE nevertheless.
       // For the case of opposite colored bishops, switch to NNUE eval with
       // small probability if the classical eval is less than the threshold.
-      if (   largePsq && !strongClassical
+      if (   largePsq
+          && !strongClassical
           && (   abs(v) * 16 < NNUEThreshold2 * r50
               || (   pos.opposite_bishops()
                   && abs(v) * 16 < (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50
                   && !(pos.this_thread()->nodes & 0xB))))
-          v = adjusted_NNUE();
+          v = (v + adjusted_NNUE() * 31) / 32;
   }
 
   // Damp down the evaluation linearly when shuffling
