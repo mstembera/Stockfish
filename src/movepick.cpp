b4b0d310c0fffc9eaf27f8a51c1b84791c5d7d02
mstembera <MissingEmail@email>
2021-03-22 15:04:13 -0700

    Dynamically adjust limit as we sort. bench: 4549347

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -35,9 +35,14 @@ namespace {
   // a given limit. The order of moves smaller than the limit is left unspecified.
   void partial_insertion_sort(ExtMove* begin, ExtMove* end, int limit) {
 
+    limit *= 10;
+    int cnt = 10;
     for (ExtMove *sortedEnd = begin, *p = begin + 1; p < end; ++p)
-        if (p->value >= limit)
+        if (p->value * cnt >= limit)
         {
+            cnt++;
+            limit += p->value;
+
             ExtMove tmp = *p, *q;
             *p = *++sortedEnd;
             for (q = sortedEnd; q != begin && *(q - 1) < tmp; --q)
