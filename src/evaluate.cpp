8386eadb3e5c64a746658b8cb4418e024273e240
mstembera <MissingEmail@email>
2021-03-28 19:51:58 -0700

    Take 2 bench: 4161709

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1105,7 +1105,7 @@ Value Eval::evaluate(const Position& pos) {
       };
 
       // If there is PSQ imbalance use classical eval, with small probability if it is small
-      Value psq = Value(abs(mg_value(pos.psq_score()) + eg_value(pos.psq_score()) * 3) / 4);
+      Value psq = Value(abs(mg_value(pos.psq_score()) + eg_value(pos.psq_score()) * 7) / 8);
       int   r50 = 16 + pos.rule50_count();
       bool  largePsq = psq * 16 > (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50;
       bool  classical = largePsq || (psq > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB));
