4d37c2727aa1bce1f907c71878b3dad6e73322db
mstembera <MissingEmail@email>
2021-08-29 00:04:15 -0700

    Tweak quiet sort bench: 5317933

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -44,6 +44,8 @@ namespace {
                 *q = *(q - 1);
             *q = tmp;
         }
+        else
+            limit -= 100;
   }
 
 } // namespace
@@ -202,7 +204,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -3000 * depth);
+          partial_insertion_sort(cur, endMoves, -3000 * depth + 200);
       }
 
       ++stage;
