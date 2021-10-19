6e7b8b96e238d3032bc0889058e5e012e2d76275
mstembera <MissingEmail@email>
2021-10-18 18:28:05 -0700

    Update up to 3 accumulators at a time instead of just 2. No functional change. bench: 5005810

--- src/nnue/nnue_feature_transformer.h
+++ src/nnue/nnue_feature_transformer.h
@@ -381,53 +381,67 @@ namespace Stockfish::Eval::NNUE {
 
       // Look for a usable accumulator of an earlier position. We keep track
       // of the estimated gain in terms of features to be added/subtracted.
-      StateInfo *st = pos.state(), *next = nullptr;
+
+      constexpr int stN = 16;
+      StateInfo* stl[stN];
+      stl[0] = pos.state();
+
+      int stID = 0;
       int gain = FeatureSet::refresh_cost(pos);
-      while (st->previous && !st->accumulator.computed[perspective])
+
+      while (   stID < stN - 1
+             && stl[stID]->previous
+             && !stl[stID]->accumulator.computed[perspective])
       {
         // This governs when a full feature refresh is needed and how many
         // updates are better than just one full refresh.
-        if (   FeatureSet::requires_refresh(st, perspective)
-            || (gain -= FeatureSet::update_cost(st) + 1) < 0)
+        if (   FeatureSet::requires_refresh(stl[stID], perspective)
+            || (gain -= FeatureSet::update_cost(stl[stID]) + 1) < 0)
           break;
-        next = st;
-        st = st->previous;
+
+        stl[stID + 1] = stl[stID]->previous;
+        ++stID;
       }
 
-      if (st->accumulator.computed[perspective])
+      if (stl[stID]->accumulator.computed[perspective])
       {
-        if (next == nullptr)
+        if (stID == 0)
           return;
 
-        // Update incrementally in two steps. First, we update the "next"
-        // accumulator. Then, we update the current accumulator (pos.state()).
+        StateInfo* baseSt = stl[stID];
+        --stID;
 
         // Gather all features to be updated.
-        const Square ksq = pos.square<KING>(perspective);
-        IndexList removed[2], added[2];
-        FeatureSet::append_changed_indices(
-          ksq, next, perspective, removed[0], added[0]);
-        for (StateInfo *st2 = pos.state(); st2 != next; st2 = st2->previous)
-          FeatureSet::append_changed_indices(
-            ksq, st2, perspective, removed[1], added[1]);
+        const Square ksq = pos.square<KING>(perspective); 
+        IndexList removed[stN], added[stN];
+        
+        constexpr int maxUpdateCnt = 3;
+        for(int i = 0; i <= std::min(stID, maxUpdateCnt - 1); ++i)
+        {
+            FeatureSet::append_changed_indices(
+              ksq, stl[i], perspective, removed[i], added[i]);
 
-        // Mark the accumulators as computed.
-        next->accumulator.computed[perspective] = true;
-        pos.state()->accumulator.computed[perspective] = true;
+            stl[i]->accumulator.computed[perspective] = true;
+        }
 
-        // Now update the accumulators listed in states_to_update[], where the last element is a sentinel.
-        StateInfo *states_to_update[3] =
-          { next, next == pos.state() ? nullptr : pos.state(), nullptr };
+        for (int i = maxUpdateCnt; i <= stID; ++i)
+            FeatureSet::append_changed_indices(
+                ksq, stl[i], perspective, removed[maxUpdateCnt - 1], added[maxUpdateCnt - 1]);
+            
+        stID = std::min(stID, maxUpdateCnt - 1);
+
+        // Now update the accumulators
   #ifdef VECTOR
         for (IndexType j = 0; j < HalfDimensions / TileHeight; ++j)
         {
           // Load accumulator
           auto accTile = reinterpret_cast<vec_t*>(
-            &st->accumulator.accumulation[perspective][j * TileHeight]);
+              &baseSt->accumulator.accumulation[perspective][j * TileHeight]);
+
           for (IndexType k = 0; k < NumRegs; ++k)
             acc[k] = vec_load(&accTile[k]);
 
-          for (IndexType i = 0; states_to_update[i]; ++i)
+          for (int i = stID; i >= 0; --i)
           {
             // Difference calculation for the deactivated features
             for (const auto index : removed[i])
@@ -449,7 +463,7 @@ namespace Stockfish::Eval::NNUE {
 
             // Store accumulator
             accTile = reinterpret_cast<vec_t*>(
-              &states_to_update[i]->accumulator.accumulation[perspective][j * TileHeight]);
+              &stl[i]->accumulator.accumulation[perspective][j * TileHeight]);
             for (IndexType k = 0; k < NumRegs; ++k)
               vec_store(&accTile[k], acc[k]);
           }
@@ -459,11 +473,12 @@ namespace Stockfish::Eval::NNUE {
         {
           // Load accumulator
           auto accTilePsqt = reinterpret_cast<psqt_vec_t*>(
-            &st->accumulator.psqtAccumulation[perspective][j * PsqtTileHeight]);
+              &baseSt->accumulator.psqtAccumulation[perspective][j * PsqtTileHeight]);
+
           for (std::size_t k = 0; k < NumPsqtRegs; ++k)
             psqt[k] = vec_load_psqt(&accTilePsqt[k]);
 
-          for (IndexType i = 0; states_to_update[i]; ++i)
+          for (int i = stID; i >= 0; --i)
           {
             // Difference calculation for the deactivated features
             for (const auto index : removed[i])
@@ -485,23 +500,23 @@ namespace Stockfish::Eval::NNUE {
 
             // Store accumulator
             accTilePsqt = reinterpret_cast<psqt_vec_t*>(
-              &states_to_update[i]->accumulator.psqtAccumulation[perspective][j * PsqtTileHeight]);
+              &stl[i]->accumulator.psqtAccumulation[perspective][j * PsqtTileHeight]);
             for (std::size_t k = 0; k < NumPsqtRegs; ++k)
               vec_store_psqt(&accTilePsqt[k], psqt[k]);
           }
         }
 
   #else
-        for (IndexType i = 0; states_to_update[i]; ++i)
+        for (int i = stID; i >= 0; --i)
         {
-          std::memcpy(states_to_update[i]->accumulator.accumulation[perspective],
-              st->accumulator.accumulation[perspective],
+          std::memcpy(&stl[i]->accumulator.accumulation[perspective],
+              baseSt->accumulator.accumulation[perspective],
               HalfDimensions * sizeof(BiasType));
 
           for (std::size_t k = 0; k < PSQTBuckets; ++k)
-            states_to_update[i]->accumulator.psqtAccumulation[perspective][k] = st->accumulator.psqtAccumulation[perspective][k];
+              stl[i]->accumulator.psqtAccumulation[perspective][k] = baseSt->accumulator.psqtAccumulation[perspective][k];
 
-          st = states_to_update[i];
+          baseSt = stl[i];
 
           // Difference calculation for the deactivated features
           for (const auto index : removed[i])
@@ -509,10 +524,10 @@ namespace Stockfish::Eval::NNUE {
             const IndexType offset = HalfDimensions * index;
 
             for (IndexType j = 0; j < HalfDimensions; ++j)
-              st->accumulator.accumulation[perspective][j] -= weights[offset + j];
+                baseSt->accumulator.accumulation[perspective][j] -= weights[offset + j];
 
             for (std::size_t k = 0; k < PSQTBuckets; ++k)
-              st->accumulator.psqtAccumulation[perspective][k] -= psqtWeights[index * PSQTBuckets + k];
+                baseSt->accumulator.psqtAccumulation[perspective][k] -= psqtWeights[index * PSQTBuckets + k];
           }
 
           // Difference calculation for the activated features
@@ -521,10 +536,10 @@ namespace Stockfish::Eval::NNUE {
             const IndexType offset = HalfDimensions * index;
 
             for (IndexType j = 0; j < HalfDimensions; ++j)
-              st->accumulator.accumulation[perspective][j] += weights[offset + j];
+                baseSt->accumulator.accumulation[perspective][j] += weights[offset + j];
 
             for (std::size_t k = 0; k < PSQTBuckets; ++k)
-              st->accumulator.psqtAccumulation[perspective][k] += psqtWeights[index * PSQTBuckets + k];
+                baseSt->accumulator.psqtAccumulation[perspective][k] += psqtWeights[index * PSQTBuckets + k];
           }
         }
   #endif
