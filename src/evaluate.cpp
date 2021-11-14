24a2df8506c1a502f346088fadabf6c85a70beeb
mstembera <MissingEmail@email>
2021-11-14 11:14:20 -0800

    Take 2 bench: 6765817

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1092,7 +1092,7 @@ Value Eval::evaluate(const Position& pos) {
   else
   {
       int scale =   898
-                  + 24 * std::min(pos.count<PAWN>(), 12)
+                  + 27 * std::min(pos.count<PAWN>(), 12)
                   + 33 * pos.non_pawn_material() / 1024;
 
        v = NNUE::evaluate(pos, true) * scale / 1024;  // NNUE
