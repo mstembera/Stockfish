6759c245c4a1e6e7cab59015121c9690388b6038
mstembera <MissingEmail@email>
2021-12-18 15:31:38 -0800

    Try to simplify based on https://tests.stockfishchess.org/tests/view/61bc1a3057a0d0f327c32a44 bench: 4531841

--- src/search.cpp
+++ src/search.cpp
@@ -1125,13 +1125,6 @@ moves_loop: // When in check, search starts here
                && abs(ss->staticEval) > 100)
           extension = 1;
 
-      // Quiet ttMove extensions
-      else if (   PvNode
-               && move == ttMove
-               && move == ss->killers[0]
-               && (*contHist[0])[movedPiece][to_sq(move)] >= 10000)
-          extension = 1;
-
       // Add extension to new depth
       newDepth += extension;
       ss->doubleExtensions = (ss-1)->doubleExtensions + (extension == 2);
