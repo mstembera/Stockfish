0f969b8963079d5f2a4649dc1ec7f4786e1b4e25
mstembera <MissingEmail@email>
2022-01-11 13:34:21 -0800

    Tweak complexity to use mg bench: 4889187

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1100,7 +1100,7 @@ Value Eval::evaluate(const Position& pos) {
        int scale      = 1136 + 20 * pos.non_pawn_material() / 1024;
        Color stm      = pos.side_to_move();
        Value optimism = pos.this_thread()->optimism[stm];
-       Value psq      = (stm == WHITE ? 1 : -1) * eg_value(pos.psq_score());
+       Value psq      = (stm == WHITE ? 1 : -1) * mg_value(pos.psq_score());
        int complexity = 35 * abs(nnue - psq) / 256;
 
        optimism = optimism * (44 + complexity) / 32;
