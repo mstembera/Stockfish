326353c4030a0b39fa5d61df0cd0b0544ef2e0ed
mstembera <MissingEmail@email>
2022-03-20 12:12:47 -0700

    Tweak nnue usage bench: 11212945

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1088,7 +1088,7 @@ Value Eval::evaluate(const Position& pos) {
   // Deciding between classical and NNUE eval (~10 Elo): for high PSQ imbalance we use classical,
   // but we switch to NNUE during long shuffling or with high material on the board.
   if (  !useNNUE
-      || abs(eg_value(pos.psq_score())) * 5 > (856 + pos.non_pawn_material() / 64) * (5 + pos.rule50_count()))
+      || abs(eg_value(pos.psq_score())) * 8 > (856 + pos.non_pawn_material() / 64) * (8 + pos.rule50_count()))
   {
       v = Evaluation<NO_TRACE>(pos).value();          // classical
       useClassical = abs(v) >= 297;
