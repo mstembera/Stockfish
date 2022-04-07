6b65d8330c6c8528b74f921380be5f1f0a52c6bf
mstembera <MissingEmail@email>
2022-04-06 22:48:09 -0700

    Calculate complexity directly from nnue bench: 7112691

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1098,12 +1098,12 @@ Value Eval::evaluate(const Position& pos) {
   // If result of a classical evaluation is much lower than threshold fall back to NNUE
   if (useNNUE && !useClassical)
   {
-       Value nnue     = NNUE::evaluate(pos, true);     // NNUE
+       int complexity;
+       Value nnue     = NNUE::evaluate(pos, true, &complexity);     // NNUE
+       complexity     = 35 * complexity / 256;
        int scale      = 1036 + 20 * pos.non_pawn_material() / 1024;
        Color stm      = pos.side_to_move();
        Value optimism = pos.this_thread()->optimism[stm];
-       Value psq      = (stm == WHITE ? 1 : -1) * eg_value(pos.psq_score());
-       int complexity = 35 * abs(nnue - psq) / 256;
 
        optimism = optimism * (44 + complexity) / 31;
        v = (nnue + optimism) * scale / 1024 - optimism;
