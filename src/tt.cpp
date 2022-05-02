03c18e71b4ad742b6130d4e0cf1843c816bd64b5
mstembera <MissingEmail@email>
2022-05-02 03:16:03 -0700

    Include generation in TTEntry::save() logic. bench: 7729968

--- src/tt.cpp
+++ src/tt.cpp
@@ -42,7 +42,8 @@ void TTEntry::save(Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev) 
   // Overwrite less valuable entries (cheapest checks first)
   if (   b == BOUND_EXACT
       || (uint16_t)k != key16
-      || d - DEPTH_OFFSET + 2 * pv > depth8 - 4)
+      ||   d - DEPTH_OFFSET + 2 * pv
+         > depth8 - 4 - ((TranspositionTable::GENERATION_CYCLE + TT.generation8 - genBound8) & TranspositionTable::GENERATION_MASK))
   {
       assert(d > DEPTH_OFFSET);
       assert(d < 256 + DEPTH_OFFSET);
