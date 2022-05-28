ba3d62426db158652326de97b772a00ed9be15e8
mstembera <MissingEmail@email>
2022-05-27 20:34:48 -0700

    Use bound in TT::save() bench: 5691817

--- src/tt.cpp
+++ src/tt.cpp
@@ -40,8 +40,8 @@ void TTEntry::save(Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev) 
       move16 = (uint16_t)m;
 
   // Overwrite less valuable entries (cheapest checks first)
-  if (   b == BOUND_EXACT
-      || (uint16_t)k != key16
+  if (   (uint16_t)k != key16
+      || b != bound()
       || d - DEPTH_OFFSET + 2 * pv > depth8 - 4)
   {
       assert(d > DEPTH_OFFSET);
