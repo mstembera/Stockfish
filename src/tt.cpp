d6cac218b4f07ac8e6d9d08cca3f623c1d812220
mstembera <MissingEmail@email>
2022-05-27 19:31:44 -0700

    Use new move in TT::save() bench: 6574487

--- src/tt.cpp
+++ src/tt.cpp
@@ -39,10 +39,10 @@ void TTEntry::save(Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev) 
   if (m || (uint16_t)k != key16)
       move16 = (uint16_t)m;
 
-  // Overwrite less valuable entries (cheapest checks first)
-  if (   b == BOUND_EXACT
-      || (uint16_t)k != key16
-      || d - DEPTH_OFFSET + 2 * pv > depth8 - 4)
+  // Overwrite less valuable entries
+  if (   (uint16_t)k != key16
+      || d - DEPTH_OFFSET + 2 * pv + bool(m) > depth8 - 4
+      || b == BOUND_EXACT)
   {
       assert(d > DEPTH_OFFSET);
       assert(d < 256 + DEPTH_OFFSET);
