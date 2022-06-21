b969027b76be15f8442c15bd6c2f1b38fc11c5df
mstembera <MissingEmail@email>
2022-06-20 22:21:52 -0700

    Score queen promotion in captures bench: 5648166

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -134,7 +134,8 @@ void MovePicker::score() {
   for (auto& m : *this)
       if constexpr (Type == CAPTURES)
           m.value =  6 * int(PieceValue[MG][pos.piece_on(to_sq(m))])
-                   +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
+                   +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))]
+                   + 6 * int(QueenValueMg - PawnValueMg) * (promotion_type(m) == QUEEN);
 
       else if constexpr (Type == QUIETS)
           m.value =      (*mainHistory)[pos.side_to_move()][from_to(m)]
