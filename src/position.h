3fb8a75f3870ee502ab66b1c02f5c3a407c5478d
mstembera <MissingEmail@email>
2022-08-17 17:47:49 -0700

    Take 2 bench: 5563869

--- src/position.h
+++ src/position.h
@@ -335,8 +335,8 @@ inline Key Position::key() const {
 template<bool AfterMove>
 inline Key Position::adjust_key50(Key k) const
 {
-  return st->rule50 < 11 - AfterMove
-      ? k : k ^ make_key((st->rule50 - (11 - AfterMove)) / 8);
+  return st->rule50 < 17 - AfterMove
+      ? k : k ^ make_key((st->rule50 - (17 - AfterMove)) / 8);
 }
 
 inline Key Position::pawn_key() const {
