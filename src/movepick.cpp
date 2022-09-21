be751a8d8aaf348215b3b867eaeb441d92dc7134
mstembera <MissingEmail@email>
2022-09-20 20:34:54 -0700

    Try checks idea in captures bench: 4263694

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -126,7 +126,8 @@ void MovePicker::score() {
   for (auto& m : *this)
       if constexpr (Type == CAPTURES)
           m.value =  6 * int(PieceValue[MG][pos.piece_on(to_sq(m))])
-                   +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
+                   +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))]
+                   +     bool(pos.check_squares(type_of(pos.moved_piece(m))) & to_sq(m)) * 128;
 
       else if constexpr (Type == QUIETS)
           m.value =  2 * (*mainHistory)[pos.side_to_move()][from_to(m)]
