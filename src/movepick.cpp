d38752a810727ee03641ff55a994e9fc55bc5de4
mstembera <MissingEmail@email>
2022-09-22 16:47:28 -0700

    Deprioritize king moves slightly because they require a full accumulator refresh bench: 4379099

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -140,7 +140,8 @@ void MovePicker::score() {
                           :                                         !(to_sq(m) & threatenedByPawn)  ? 15000
                           :                                                                           0)
                           :                                                                           0)
-                   +     bool(pos.check_squares(type_of(pos.moved_piece(m))) & to_sq(m)) * 16384;
+                   +     bool(pos.check_squares(type_of(pos.moved_piece(m))) & to_sq(m)) * 16384
+                   -     (type_of(pos.moved_piece(m)) == KING);
       else // Type == EVASIONS
       {
           if (pos.capture(m))
