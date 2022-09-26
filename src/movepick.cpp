591ea381aee4a918e0281544cede2ba7269e6294
mstembera <MissingEmail@email>
2022-09-26 01:30:21 -0700

    Take capturing piece into account w/o changing average value bench: 4052682

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -125,7 +125,8 @@ void MovePicker::score() {
 
   for (auto& m : *this)
       if constexpr (Type == CAPTURES)
-          m.value =  6 * int(PieceValue[MG][pos.piece_on(to_sq(m))])
+          m.value =  7 * int(PieceValue[MG][pos.piece_on(to_sq(m))])
+                   -     int(PieceValue[MG][pos.moved_piece(m)])
                    +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if constexpr (Type == QUIETS)
