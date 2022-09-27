3d2d47f2ae64a855e26698a2e17e524161af889e
mstembera <MissingEmail@email>
2022-09-26 17:20:25 -0700

    Take 3 bench: 4422581

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -125,8 +125,8 @@ void MovePicker::score() {
 
   for (auto& m : *this)
       if constexpr (Type == CAPTURES)
-          m.value =  (  13 * int(PieceValue[MG][pos.piece_on(to_sq(m))])
-                      -      int(PieceValue[MG][pos.moved_piece(m)])) / 2
+          m.value =  8 * int(PieceValue[MG][pos.piece_on(to_sq(m))])
+                   - 2 * int(PieceValue[MG][pos.moved_piece(m)])
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if constexpr (Type == QUIETS)
