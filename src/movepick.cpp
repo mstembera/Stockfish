3ca79ef0d59913a6a2d32a80043a6ddd627bd660
mstembera <MissingEmail@email>
2022-10-05 01:50:53 -0700

    Add undefended pieces to threatened bench: 4293036

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -107,7 +107,7 @@ void MovePicker::score() {
 
   static_assert(Type == CAPTURES || Type == QUIETS || Type == EVASIONS, "Wrong type");
 
-  [[maybe_unused]] Bitboard threatenedByPawn, threatenedByMinor, threatenedByRook;
+  [[maybe_unused]] Bitboard threatenedByPawn, threatenedByMinor, threatenedByRook, threatenedByAny;
   if constexpr (Type == QUIETS)
   {
       Color us = pos.side_to_move();
@@ -115,11 +115,18 @@ void MovePicker::score() {
       threatenedByPawn  = pos.attacks_by<PAWN>(~us);
       threatenedByMinor = pos.attacks_by<KNIGHT>(~us) | pos.attacks_by<BISHOP>(~us) | threatenedByPawn;
       threatenedByRook  = pos.attacks_by<ROOK>(~us) | threatenedByMinor;
+      threatenedByAny   = pos.attacks_by<QUEEN>(~us) | pos.attacks_by<KING>(~us) | threatenedByRook;
 
       // Pieces threatened by pieces of lesser material value
       threatenedPieces = (pos.pieces(us, QUEEN) & threatenedByRook)
                        | (pos.pieces(us, ROOK)  & threatenedByMinor)
                        | (pos.pieces(us, KNIGHT, BISHOP) & threatenedByPawn);
+
+      Bitboard undefended =  pos.attacks_by<PAWN>(us)   | pos.attacks_by<KNIGHT>(us)
+                           | pos.attacks_by<BISHOP>(us) | pos.attacks_by<ROOK>(us)
+                           | pos.attacks_by<QUEEN>(us)  | pos.attacks_by<KING>(us);
+      undefended = ~undefended & pos.pieces(us) & threatenedByAny;
+      threatenedPieces |= undefended; 
   }
 
   for (auto& m : *this)
