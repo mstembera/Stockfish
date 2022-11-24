cb7fcd0a1a9a880807c2bf29cdce7ebe7eeefbd1
mstembera <MissingEmail@email>
2022-11-24 14:21:42 -0800

    Variant of an original idea by @pb00067 bench: 4390318

--- src/thread.cpp
+++ src/thread.cpp
@@ -19,6 +19,7 @@
 #include <cassert>
 
 #include <algorithm> // For std::count
+#include <unordered_map>
 #include "movegen.h"
 #include "search.h"
 #include "thread.h"
@@ -213,19 +214,20 @@ void ThreadPool::start_thinking(Position& pos, StateListPtr& states,
 Thread* ThreadPool::get_best_thread() const {
 
     Thread* bestThread = front();
-    std::map<Move, int64_t> votes;
+    std::unordered_map<Move, int64_t> votes(2 * std::min(size(), bestThread->rootMoves.size()));
     Value minScore = VALUE_NONE;
 
     // Find minimum score of all threads
-    for (Thread* th: *this)
+    for (Thread* th : *this)
         minScore = std::min(minScore, th->rootMoves[0].score);
 
     // Vote according to score and depth, and select the best thread
     for (Thread* th : *this)
-    {
         votes[th->rootMoves[0].pv[0]] +=
-            (th->rootMoves[0].score - minScore + 14) * int(th->completedDepth);
+              (th->rootMoves[0].score - minScore + 14)
+            * int(th->completedDepth + th->rootMoves[0].scoreLowerbound - 4 * th->rootMoves[0].scoreUpperbound);
 
+    for (Thread* th : *this)
         if (abs(bestThread->rootMoves[0].score) >= VALUE_TB_WIN_IN_MAX_PLY)
         {
             // Make sure we pick the shortest mate / TB conversion or stave off mate the longest
@@ -238,7 +240,6 @@ Thread* ThreadPool::get_best_thread() const {
                          || (   votes[th->rootMoves[0].pv[0]] == votes[bestThread->rootMoves[0].pv[0]]
                              && th->rootMoves[0].pv.size() > bestThread->rootMoves[0].pv.size()))))
             bestThread = th;
-    }
 
     return bestThread;
 }
