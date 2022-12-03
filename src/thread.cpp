5e9e3f899c9ffad7c9f914db5a390bd8f6006e67
mstembera <MissingEmail@email>
2022-12-03 13:53:22 -0800

    Take 2 Opposite direction bench: 3467381

--- src/thread.cpp
+++ src/thread.cpp
@@ -222,7 +222,7 @@ Thread* ThreadPool::get_best_thread() const {
 
     // Vote according to score and depth, and select the best thread
     auto thread_value = [minScore](const Thread* th) {
-        return (th->rootMoves[0].score - minScore + 40) * int(th->completedDepth);
+        return (th->rootMoves[0].score - minScore + 5) * int(th->completedDepth);
     };
 
     for (Thread* th : *this)
