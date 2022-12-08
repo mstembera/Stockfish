b761c58679dcf794353015de9d75422d3852d8ae
mstembera <MissingEmail@email>
2022-12-07 21:22:50 -0800

    Change alpha beta update order bench: 3646176

--- src/search.cpp
+++ src/search.cpp
@@ -398,8 +398,8 @@ void Thread::search() {
               // re-search, otherwise exit the loop.
               if (bestValue <= alpha)
               {
-                  beta = (alpha + beta) / 2;
                   alpha = std::max(bestValue - delta, -VALUE_INFINITE);
+                  beta = (alpha + beta) / 2;
 
                   failedHighCnt = 0;
                   if (mainThread)
