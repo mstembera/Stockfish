89b157514b3d6654054e9182a953b37245cfd08a
mstembera <MissingEmail@email>
2022-12-27 16:28:22 -0800

    Quick early check of ttMove   Take 2 bench: 3842303

--- src/search.cpp
+++ src/search.cpp
@@ -619,12 +619,12 @@ namespace {
     excludedMove = ss->excludedMove;
     posKey = excludedMove == MOVE_NONE ? pos.key() : pos.key() ^ make_key(excludedMove);
     tte = TT.probe(posKey, ss->ttHit);
-    if (   ss->ttHit && tte->move()
-        && (pos.piece_on(from_sq(tte->move())) == NO_PIECE || color_of(pos.piece_on(from_sq(tte->move()))) != us))
-        ss->ttHit = false;
     ttValue = ss->ttHit ? value_from_tt(tte->value(), ss->ply, pos.rule50_count()) : VALUE_NONE;
     ttMove =  rootNode ? thisThread->rootMoves[thisThread->pvIdx].pv[0]
-            : ss->ttHit    ? tte->move() : MOVE_NONE;
+            :   ss->ttHit
+             && tte->move()
+             && pos.piece_on(from_sq(tte->move())) != NO_PIECE
+             && color_of(pos.piece_on(from_sq(tte->move()))) == us ? tte->move() : MOVE_NONE;
     ttCapture = ttMove && pos.capture(ttMove);
     if (!excludedMove)
         ss->ttPv = PvNode || (ss->ttHit && tte->is_pv());
@@ -1441,11 +1441,11 @@ moves_loop: // When in check, search starts here
     // Transposition table lookup
     posKey = pos.key();
     tte = TT.probe(posKey, ss->ttHit);
-    if (   ss->ttHit && tte->move() 
-        && (pos.piece_on(from_sq(tte->move())) == NO_PIECE || color_of(pos.piece_on(from_sq(tte->move()))) != pos.side_to_move()))
-        ss->ttHit = false;
     ttValue = ss->ttHit ? value_from_tt(tte->value(), ss->ply, pos.rule50_count()) : VALUE_NONE;
-    ttMove = ss->ttHit ? tte->move() : MOVE_NONE;
+    ttMove =   ss->ttHit
+            && tte->move()
+            && pos.piece_on(from_sq(tte->move())) != NO_PIECE
+            && color_of(pos.piece_on(from_sq(tte->move()))) == pos.side_to_move() ? tte->move() : MOVE_NONE;
     pvHit = ss->ttHit && tte->is_pv();
 
     if (  !PvNode
