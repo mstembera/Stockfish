da3642dbebe29cdc1c46bdf3d22eb4f7c2bf11fe
mstembera <MissingEmail@email>
2023-08-09 06:32:41 -0700

    Take 2 bench: 1573024

--- src/nnue/layers/affine_transform_sparse_input.h
+++ src/nnue/layers/affine_transform_sparse_input.h
@@ -241,7 +241,7 @@ namespace Stockfish::Eval::NNUE::Layers {
         acc[k] = biasvec[k];
 
       IndexType j = 0;
-      for (; j < count - 1; j += 2)
+      for (; j < count - 2; j += 3)
       {
         const auto i1 = nnz[j];
         const invec_t in1 = vec_set_32(input32[i1]);
@@ -254,6 +254,12 @@ namespace Stockfish::Eval::NNUE::Layers {
 		const auto col2 = reinterpret_cast<const invec_t*>(&weights[i2 * OutputDimensions * ChunkSize]);
 		for (IndexType k = 0; k < NumRegs; ++k)
 			vec_add_dpbusd_32(acc[k], in2, col2[k]);
+
+		const auto i3 = nnz[j + 2];
+		const invec_t in3 = vec_set_32(input32[i3]);
+		const auto col3 = reinterpret_cast<const invec_t*>(&weights[i3 * OutputDimensions * ChunkSize]);
+		for (IndexType k = 0; k < NumRegs; ++k)
+			vec_add_dpbusd_32(acc[k], in3, col3[k]);
       }
       for (; j < count; j++)
       {
