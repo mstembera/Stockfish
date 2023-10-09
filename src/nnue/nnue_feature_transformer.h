5ccafe52a4dc97704d33cf4adbfe57a6d8e98fd3
mstembera <m_stembera@yahoo.com>
2023-10-09 13:19:36 -0700

    Adjust cost when updating only one accumulator (opposite) bench: 1246560

--- src/nnue/nnue_feature_transformer.h
+++ src/nnue/nnue_feature_transformer.h
@@ -321,7 +321,7 @@ namespace Stockfish::Eval::NNUE {
       // Look for a usable accumulator of an earlier position. We keep track
       // of the estimated gain in terms of features to be added/subtracted.
       StateInfo *st = pos.state(), *next = nullptr;
-      int gain = FeatureSet::refresh_cost(pos) + 10;
+      int gain = FeatureSet::refresh_cost(pos) - 5;
       while (st->previous && !st->accumulator.computed[Perspective])
       {
         // This governs when a full feature refresh is needed and how many
@@ -329,8 +329,7 @@ namespace Stockfish::Eval::NNUE {
         if (   FeatureSet::requires_refresh(st, Perspective)
             || (gain -= FeatureSet::update_cost(st) + 1) < 0)
           break;
-
-        gain -= 10 * (next == nullptr);
+        gain += 10 * (next == nullptr);
         next = st;
         st = st->previous;
       }
