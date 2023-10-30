6549a355f0583e1741e76615ca8bd85d2476cfe2
mstembera <m_stembera@yahoo.com>
2023-10-30 02:30:37 -0700

    Revert delta scale part of https://github.com/official-stockfish/Stockfish/pull/4853 bench: 1563098

--- src/search.cpp
+++ src/search.cpp
@@ -364,7 +364,7 @@ void Thread::search() {
 
             // Reset aspiration window starting size
             Value avg = rootMoves[pvIdx].averageScore;
-            delta     = Value(10) + int(avg) * avg / 15335;
+            delta     = Value(10) + int(avg) * avg / 17470;
             alpha     = std::max(avg - delta, -VALUE_INFINITE);
             beta      = std::min(avg + delta, VALUE_INFINITE);
 
