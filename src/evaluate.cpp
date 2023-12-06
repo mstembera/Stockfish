0d5a34ff0016fa2f1fca8131d08cad8cc3217116
mstembera <m_stembera@yahoo.com>
2023-12-06 09:20:50 -0800

    Take 4 bench: 1240063

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -167,18 +167,18 @@ Value Eval::evaluate(const Position& pos) {
     int notUpdated = 0;
     for (const StateInfo* st = pos.state(); st != nullptr && !st->accumulator.computed[0]; st = st->previous)
     {
+        ++notUpdated;
         if (type_of(st->dirtyPiece.piece[0]) == KING)
         {
-            notUpdated = pos.count<ALL_PIECES>() / 4;
+            notUpdated += pos.count<ALL_PIECES>() / 4;
             break;
         }
-        ++notUpdated;
     }
 
     bool lazy = abs(simpleEval) >= RookValue + KnightValue + 16 * shuffling * shuffling
                                      + abs(pos.this_thread()->bestValue)
                                      + abs(pos.this_thread()->rootSimpleEval)
-                                     - notUpdated * 128 + 128;
+                                     - notUpdated * 100 + 100;
 
     if (lazy)
         v = Value(simpleEval);
