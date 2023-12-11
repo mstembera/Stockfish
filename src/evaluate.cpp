d7fcd6509a33b825e8666f3f777418f73d20b9dd
mstembera <m_stembera@yahoo.com>
2023-12-10 18:51:37 -0800

    Include piece count in lazy bench: 1404041

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -166,7 +166,8 @@ Value Eval::evaluate(const Position& pos) {
 
     bool lazy = abs(simpleEval) >= RookValue + KnightValue + 16 * shuffling * shuffling
                                      + abs(pos.this_thread()->bestValue)
-                                     + abs(pos.this_thread()->rootSimpleEval);
+                                     + abs(pos.this_thread()->rootSimpleEval)
+                                     + pos.count<ALL_PIECES>() * 8 - 128;
 
     if (lazy)
         v = Value(simpleEval);
