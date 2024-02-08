7f2dfaea89c395120d1d5e69016c2468dc1c8e39
mstembera <m_stembera@yahoo.com>
2024-02-08 14:19:25 -0800

    Variant of https://tests.stockfishchess.org/tests/view/65c3a902c865510db0283916 by @locutus2 bench: 1663005

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -211,7 +211,8 @@ void MovePicker::score() {
         {
             if (pos.capture_stage(m))
                 m.value =
-                  PieceValue[pos.piece_on(m.to_sq())] - type_of(pos.moved_piece(m)) + (1 << 28);
+                    PieceValue[pos.piece_on(m.to_sq())] - type_of(pos.moved_piece(m))
+                  + (pos.see_ge(m, -PawnValue) ? (1 << 28) : -(1 << 28));
             else
                 m.value = (*mainHistory)[pos.side_to_move()][m.from_to()]
                         + (*continuationHistory[0])[pos.moved_piece(m)][m.to_sq()]
