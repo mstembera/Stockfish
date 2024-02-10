b47dd6419b9fb6f093283cea943df9cd85fb2d85
mstembera <m_stembera@yahoo.com>
2024-02-09 23:15:15 -0800

    Use the square of the depth for voting bench: 1116591

--- src/thread.cpp
+++ src/thread.cpp
@@ -225,7 +225,7 @@ Thread* ThreadPool::get_best_thread() const {
     // Vote according to score and depth, and select the best thread
     auto thread_voting_value = [minScore](Thread* th) {
         auto sq = [](int v) { return v * v; };
-        return sq(th->worker->rootMoves[0].score - minScore + 14) * int(th->worker->completedDepth);
+        return (th->worker->rootMoves[0].score - minScore + 14) * sq(th->worker->completedDepth);
     };
 
     for (Thread* th : threads)
