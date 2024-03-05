92427d43da28111fbc1319fb06ade14c5efbb845
mstembera <m_stembera@yahoo.com>
2024-03-04 19:28:35 -0800

    Re-evaluate smallNet evals below 350 bench: 1283770

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -201,7 +201,7 @@ Value Eval::evaluate(const Position& pos, int optimism) {
     Value nnue = smallNet ? NNUE::evaluate<NNUE::Small>(pos, true, &nnueComplexity, psqtOnly)
                           : NNUE::evaluate<NNUE::Big>(pos, true, &nnueComplexity, false);
 
-    if (smallNet && abs(nnue) < 250)
+    if (smallNet && abs(nnue) < 350)
         nnue = NNUE::evaluate<NNUE::Big>(pos, true, &nnueComplexity, false);
 
     // Blend optimism and eval with nnue complexity and material imbalance
