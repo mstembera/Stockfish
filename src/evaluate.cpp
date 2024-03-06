d5b263bcf64a98b7dc25e4a78c960de54e5d1287
mstembera <m_stembera@yahoo.com>
2024-03-06 13:38:04 -0800

    Re-evaluate smallNet evals if the sign flips or goes below 100 bench: 1306235

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -201,9 +201,8 @@ Value Eval::evaluate(const Position& pos, int optimism) {
     Value nnue = smallNet ? NNUE::evaluate<NNUE::Small>(pos, true, &nnueComplexity, psqtOnly)
                           : NNUE::evaluate<NNUE::Big>(pos, true, &nnueComplexity, false);
 
-    bool reEval = simpleEval > 0 ? nnue + 700 < simpleEval : nnue - 700 > simpleEval;
-    if (psqtOnly && reEval)
-        nnue = NNUE::evaluate<NNUE::Small>(pos, true, &nnueComplexity, false);
+    if (smallNet && (nnue * simpleEval < 0 || std::abs(nnue) < 100))
+        nnue = NNUE::evaluate<NNUE::Big>(pos, true, &nnueComplexity, false);
 
     // Blend optimism and eval with nnue complexity and material imbalance
     optimism += optimism * (nnueComplexity + std::abs(simpleEval - nnue)) / 512;
