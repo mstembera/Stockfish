304793c4e823d679bf45a2e0eb5b3dd49f0a2521
mstembera <m_stembera@yahoo.com>
2024-05-13 01:58:50 -0700

    Reeval if nnue is 3x bigger or smaller than simple eval bench: 1795069

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -64,7 +64,8 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
 
     if (   smallNet
         && (   nnue * simpleEval < 0
-            || 3 * std::abs(nnue) < std::abs((simpleEval))))
+            || 3 * std::abs(nnue) < std::abs(simpleEval)
+            || std::abs(nnue) > 3 * std::abs(simpleEval)))
     {
         nnue = networks.big.evaluate(pos, &caches.big, true, &nnueComplexity);
         smallNet = false;
