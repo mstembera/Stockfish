7c101a6cf45c04ef5968b74db95a71dbda12e1bf
mstembera <m_stembera@yahoo.com>
2024-05-13 01:54:37 -0700

    Smaller only 3x bench: 1795069

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -64,7 +64,7 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
 
     if (   smallNet
         && (   nnue * simpleEval < 0
-            || 2 * std::abs(nnue) < std::abs((simpleEval))))
+            || 3 * std::abs(nnue) < std::abs((simpleEval))))
     {
         nnue = networks.big.evaluate(pos, &caches.big, true, &nnueComplexity);
         smallNet = false;
