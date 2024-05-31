6348dc2d6aa0247328557b245a6343e6e689d7ca
mstembera <m_stembera@yahoo.com>
2024-05-31 16:46:45 -0700

    Disable VNNI Use 4096 wide net bench: 1274394

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,7 +33,7 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultNameBig "nn-ddcfb9224cdb.nnue"
+#define EvalFileDefaultNameBig "nn-9b70b2b6fdf3.nnue"
 #define EvalFileDefaultNameSmall "nn-37f18f62d772.nnue"
 
 namespace NNUE {
