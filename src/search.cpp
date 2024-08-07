9d71ad7219363547b27fce3fd30cfe038e29529f
mstembera <m_stembera@yahoo.com>
2024-08-07 13:46:19 -0700

    Take 2 bench: 1094308

--- src/search.cpp
+++ src/search.cpp
@@ -296,7 +296,7 @@ void Search::Worker::iterative_deepening() {
 
             // Reset aspiration window starting size
             Value avg = rootMoves[pvIdx].averageScore;
-            delta     = 3 + 12 / rootDepth + avg * avg / 13424;
+            delta     = 5 + 5 / rootDepth + avg * avg / 13424;
             alpha     = std::max(avg - delta, -VALUE_INFINITE);
             beta      = std::min(avg + delta, VALUE_INFINITE);
 
