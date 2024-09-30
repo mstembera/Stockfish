0c3294edcfa3349cbbb715b23b6f9ce8593124f5
mstembera <m_stembera@yahoo.com>
2024-09-30 01:41:39 -0700

    Take 4 bench: 1140842

--- src/search.cpp
+++ src/search.cpp
@@ -1200,7 +1200,7 @@ moves_loop:  // When in check, search starts here
                     value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
 
                 // Post LMR continuation history updates (~1 Elo)
-                int bonus = value >= beta ? (1 + 2 * (moveCount + 4 > depth)) * stat_bonus(newDepth)
+                int bonus = value >= beta ? (1 + 2 * moveCount > depth + moveCount > depth) * stat_bonus(newDepth)
                                           : -stat_malus(newDepth);
                 update_continuation_histories(ss, movedPiece, move.to_sq(), bonus);
             }
