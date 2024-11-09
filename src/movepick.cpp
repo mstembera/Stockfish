35f54bc924b0fe4cf2c3214b25b6827f1042677d
mstembera <m_stembera@yahoo.com>
2024-11-09 10:01:09 -0800

    Reduce capture divisor for all depths bench: 1244069

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -245,7 +245,7 @@ top:
     case GOOD_CAPTURE :
         if (select<Next>([&]() {
                 // Move losing capture to endBadCaptures to be tried later
-                return pos.see_ge(*cur, -cur->value / (18 + 2 * (depth > 2))) ? true
+                return pos.see_ge(*cur, -cur->value / 16) ? true
                                                           : (*endBadCaptures++ = *cur, false);
             }))
             return *(cur - 1);
