4ec597d42afb9ea55641d17be6a1c2ab26de51f3
mstembera <m_stembera@yahoo.com>
2024-11-12 13:20:19 -0800

    Optimize hashfull() No functional change bench: 1281912

--- src/tt.cpp
+++ src/tt.cpp
@@ -196,12 +196,11 @@ void TranspositionTable::clear(ThreadPool& threads) {
 int TranspositionTable::hashfull(int maxAge) const {
     int maxAgeInternal = maxAge << GENERATION_BITS;
     int cnt = 0;
-    for (int i = 0; i < 500; ++i)
-        for (int j = 0; j < ClusterSize; ++j)
-            cnt +=  table[i].entry[j].is_occupied()
+    for (int i = 0, eID = 0; eID < 1000; ++i)
+        for (int j = 0; j < ClusterSize && eID < 1000; ++j, ++eID)
+            cnt +=   table[i].entry[j].is_occupied()
                   && table[i].entry[j].relative_age(generation8) <= maxAgeInternal;
-
-    return cnt * 2 / ClusterSize;
+    return cnt;
 }
 
 
