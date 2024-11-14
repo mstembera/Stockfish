5354f81549ad8a548cd93831dd6044c95c527f0c
mstembera <m_stembera@yahoo.com>
2024-11-14 15:59:39 -0800

    Skip bad captures at depth 1 if also skipping quiets.  Div 17 bench: 898658

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -247,7 +247,7 @@ top:
     case GOOD_CAPTURE :
         if (select<Next>([&]() {
                 // Move losing capture to endBadCaptures to be tried later
-                return pos.see_ge(*cur, -cur->value / 18) ? true
+                return pos.see_ge(*cur, -cur->value / 17) ? true
                                                           : (*endBadCaptures++ = *cur, false);
             }))
             return *(cur - 1);
@@ -286,7 +286,7 @@ top:
         [[fallthrough]];
 
     case BAD_CAPTURE :
-        if ((!skipQuiets || depth > 2) && select<Next>([]() { return true; }))
+        if ((!skipQuiets || depth > 1) && select<Next>([]() { return true; }))
             return *(cur - 1);
 
         // Prepare the pointers to loop over the bad quiets
