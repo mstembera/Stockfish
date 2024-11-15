5036b437986ca1e1512a113c28eb79606578e176
mstembera <m_stembera@yahoo.com>
2024-11-14 19:40:04 -0800

    Skip bad captures at depth 1 if also skipping quiets. Bias -60 bench: 959647

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -247,7 +247,7 @@ top:
     case GOOD_CAPTURE :
         if (select<Next>([&]() {
                 // Move losing capture to endBadCaptures to be tried later
-                return pos.see_ge(*cur, -cur->value / 18 - 32 * (depth == 1)) ? true
+                return pos.see_ge(*cur, -cur->value / 18 - 60) ? true
                                                           : (*endBadCaptures++ = *cur, false);
             }))
             return *(cur - 1);
