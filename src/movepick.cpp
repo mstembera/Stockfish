d59505b01ffcbb9b97b66802eb35b28f3bcae2be
mstembera <m_stembera@yahoo.com>
2024-11-14 17:01:59 -0800

    Skip bad captures at depth 1 if also skipping quiets. W/ depth 1 compensation bench: 937481

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -247,7 +247,7 @@ top:
     case GOOD_CAPTURE :
         if (select<Next>([&]() {
                 // Move losing capture to endBadCaptures to be tried later
-                return pos.see_ge(*cur, -cur->value / 18 - 40) ? true
+                return pos.see_ge(*cur, -cur->value / 18 - 64 * (depth == 1)) ? true
                                                           : (*endBadCaptures++ = *cur, false);
             }))
             return *(cur - 1);
