d02a2860b247f54bcb82739f5ff5ce9269433a5c
mstembera <m_stembera@yahoo.com>
2024-11-25 18:12:38 -0800

    Take 2 bench: 998114

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -150,7 +150,7 @@ void MovePicker::score() {
               7 * int(PieceValue[pos.piece_on(m.to_sq())])
               -   int(PieceValue[pos.moved_piece(m)])
               + (*captureHistory)[pos.moved_piece(m)][m.to_sq()][type_of(pos.piece_on(m.to_sq()))]
-              + 2600;
+              + 2000;
 
         else if constexpr (Type == QUIETS)
         {
