b839dc5ffc30a17f47a5aadd15323018c98ed8ba
mstembera <m_stembera@yahoo.com>
2024-12-09 13:55:16 -0800

    Part of fixing https://github.com/official-stockfish/Stockfish/issues/5503 bench: 934447

--- src/tt.cpp
+++ src/tt.cpp
@@ -238,7 +238,16 @@ std::tuple<bool, TTData, TTWriter> TranspositionTable::probe(const Key key) cons
             > tte[i].depth8 - tte[i].relative_age(generation8) * 2)
             replace = &tte[i];
 
-    return {false, TTData(), TTWriter(replace)};
+    constexpr TTData emptyData = {
+        Move::none(),
+        VALUE_NONE,
+        VALUE_NONE,
+        DEPTH_ENTRY_OFFSET,
+        BOUND_NONE,
+        false
+    };
+
+    return {false, emptyData, TTWriter(replace)};
 }
 
 
