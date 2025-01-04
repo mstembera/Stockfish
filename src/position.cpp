eeebe8234ddcbb3c3b4b702c7855ca42855295eb
mstembera <m_stembera@yahoo.com>
2025-01-04 05:07:52 -0800

    Take 2  Clean bench: 1294909

--- src/position.cpp
+++ src/position.cpp
@@ -503,9 +503,9 @@ Bitboard Position::attackers_to(Square s, Bitboard occupied) const {
 
 bool Position::attackers_to_exist(Square s, Bitboard occupied, Color c) const {
 
-     return (  (pawn_attacks_bb(~c, s) & pieces(c, PAWN))
-             | (attacks_bb<KNIGHT>(s)  & pieces(c, KNIGHT))
-             | (attacks_bb<KING>(s)    & pieces(c, KING)))
+     return ((  (pawn_attacks_bb(~c, s) & pieces(PAWN))
+              | (attacks_bb<KNIGHT>(s)  & pieces(KNIGHT))
+              | (attacks_bb<KING>(s)    & pieces(KING))) & pieces(c))
           || ((attacks_bb<ROOK>(s)   & pieces(c, ROOK,   QUEEN)) && (attacks_bb<ROOK>(s, occupied)   & pieces(c, ROOK,   QUEEN)))
           || ((attacks_bb<BISHOP>(s) & pieces(c, BISHOP, QUEEN)) && (attacks_bb<BISHOP>(s, occupied) & pieces(c, BISHOP, QUEEN)));
 }
