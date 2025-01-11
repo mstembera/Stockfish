e56a63b1431e15117c5359ba5843f4ed8ca05866
mstembera <m_stembera@yahoo.com>
2025-01-11 13:11:54 -0800

    This will be the PR if it passes bench: 999324

--- src/position.cpp
+++ src/position.cpp
@@ -503,11 +503,11 @@ Bitboard Position::attackers_to(Square s, Bitboard occupied) const {
 
 bool Position::attackers_to_exist(Square s, Bitboard occupied, Color c) const {
 
-     return ((  (pawn_attacks_bb(~c, s) & pieces(PAWN))
+     return ((attacks_bb<ROOK>(s)   & pieces(c, ROOK,   QUEEN)) && (attacks_bb<ROOK>(s, occupied)   & pieces(c, ROOK,   QUEEN)))
+         || ((attacks_bb<BISHOP>(s) & pieces(c, BISHOP, QUEEN)) && (attacks_bb<BISHOP>(s, occupied) & pieces(c, BISHOP, QUEEN)))
+         || ((  (pawn_attacks_bb(~c, s) & pieces(PAWN))
               | (attacks_bb<KNIGHT>(s)  & pieces(KNIGHT))
-              | (attacks_bb<KING>(s)    & pieces(KING))) & pieces(c))
-          || ((attacks_bb<ROOK>(s)   & pieces(c, ROOK,   QUEEN)) && (attacks_bb<ROOK>(s, occupied)   & pieces(c, ROOK,   QUEEN)))
-          || ((attacks_bb<BISHOP>(s) & pieces(c, BISHOP, QUEEN)) && (attacks_bb<BISHOP>(s, occupied) & pieces(c, BISHOP, QUEEN)));
+              | (attacks_bb<KING>(s)    & pieces(KING))) & pieces(c));
 }
 
 // Tests whether a pseudo-legal move is legal
