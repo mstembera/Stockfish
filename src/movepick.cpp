8852081e8e6c3455bc79ab1a435a6cb2656b7380
mstembera <m_stembera@yahoo.com>
2025-01-18 19:10:47 -0800

    Good quiet threshold -9500 bench: 1314196

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -214,7 +214,7 @@ Move MovePicker::select(Pred filter) {
 // picking the move with the highest score from a list of generated moves.
 Move MovePicker::next_move() {
 
-    constexpr int good_quiet_threshold = -6500;
+    constexpr int good_quiet_threshold = -9500;
     auto sort_quiet_threshold = [](Depth d) { return -3560 * d; };
 
 top:
