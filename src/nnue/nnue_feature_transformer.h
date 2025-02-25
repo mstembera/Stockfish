c2cf5fac91ae9579c140d607c81922e8224fe277
mstembera <m_stembera@yahoo.com>
2025-02-25 14:51:02 -0800

    6 itterations bench: 2146010

--- src/nnue/nnue_feature_transformer.h
+++ src/nnue/nnue_feature_transformer.h
@@ -875,7 +875,7 @@ class FeatureTransformer {
             if (   FeatureSet::requires_refresh(st, Perspective)
                 || !st->previous
                 || st->previous->next != st
-                || loopCnt++ > 8)
+                || loopCnt++ > 6)
             {
                 // compute accumulator from scratch for this position
                 update_accumulator_refresh_cache<Perspective>(pos, cache);
