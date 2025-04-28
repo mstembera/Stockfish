9a9c4fbb22da72f4c6fccbfaee4c2ee5bfa12dfd
mstembera <m_stembera@yahoo.com>
2025-04-27 20:17:54 -0700

    Simplify check capture scoring 2048 bench: 2267156

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -146,7 +146,7 @@ void MovePicker::score() {
         if constexpr (Type == CAPTURES)
             m.value =
                   7 * int(PieceValue[pos.piece_on(m.to_sq())])
-              + 512 * bool(pos.check_squares(type_of(pos.moved_piece(m))) & m.to_sq())
+              + 2048 * bool(pos.check_squares(type_of(pos.moved_piece(m))) & m.to_sq())
               + (*captureHistory)[pos.moved_piece(m)][m.to_sq()][type_of(pos.piece_on(m.to_sq()))];
 
         else if constexpr (Type == QUIETS)
