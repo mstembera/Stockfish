98404998d269134649a642f112d49e3d99e3e576
mstembera <m_stembera@yahoo.com>
2025-05-03 14:34:50 -0700

    Try gives_check() on top of PR6047 bench: 1792574

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -146,7 +146,7 @@ void MovePicker::score() {
         if constexpr (Type == CAPTURES)
             m.value =
                 7 * int(PieceValue[pos.piece_on(m.to_sq())])
-              + 1024 * bool(pos.check_squares(type_of(pos.moved_piece(m))) & m.to_sq())
+              + 1024 * pos.gives_check(m)
               + (*captureHistory)[pos.moved_piece(m)][m.to_sq()][type_of(pos.piece_on(m.to_sq()))];
 
         else if constexpr (Type == QUIETS)
