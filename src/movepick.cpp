5fefa0e754add62542969c1778ee586214f3459f
mstembera <m_stembera@yahoo.com>
2025-05-08 19:38:40 -0700

    Tweak check scoring bonus for quiets bench: 1999145

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -169,7 +169,8 @@ void MovePicker::score() {
             m.value += (*continuationHistory[5])[pc][to];
 
             // bonus for checks
-            m.value += (bool(pos.check_squares(pt) & to) && pos.see_ge(m, -75)) * 16384;
+            if (bool(pos.check_squares(pt) & to))
+                m.value += 10000 + 5000 * pos.see_ge(m, -75);
 
             // penalty for moving to a square threatened by a lesser piece
             // or bonus for escaping an attack by a lesser piece.
