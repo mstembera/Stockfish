b3d28743f07e25444a9f63b2d70f47fe4a58982b
mstembera <m_stembera@yahoo.com>
2025-08-07 18:57:43 -0700

    Both 150 bench: 3010747

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -129,13 +129,16 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
 
     Color us = pos.side_to_move();
 
-    [[maybe_unused]] Bitboard threatByLesser[QUEEN + 1];
+    [[maybe_unused]] Bitboard threatByLesser[KING + 1];
     if constexpr (Type == QUIETS)
     {
+        threatByLesser[PAWN] = 0;
         threatByLesser[KNIGHT] = threatByLesser[BISHOP] = pos.attacks_by<PAWN>(~us);
         threatByLesser[ROOK] =
           pos.attacks_by<KNIGHT>(~us) | pos.attacks_by<BISHOP>(~us) | threatByLesser[KNIGHT];
         threatByLesser[QUEEN] = pos.attacks_by<ROOK>(~us) | threatByLesser[ROOK];
+        threatByLesser[KING] =
+          pos.attacks_by<QUEEN>(~us) | pos.attacks_by<KING>(~us) | threatByLesser[QUEEN];
     }
 
     ExtMove* it = cur;
@@ -170,12 +173,9 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
 
             // penalty for moving to a square threatened by a lesser piece
             // or bonus for escaping an attack by a lesser piece.
-            if (KNIGHT <= pt && pt <= QUEEN)
-            {
-                static constexpr int bonus[QUEEN + 1] = {0, 0, 144, 144, 256, 517};
-                int v = threatByLesser[pt] & to ? -95 : 100 * bool(threatByLesser[pt] & from);
-                m.value += bonus[pt] * v;
-            }
+            static constexpr int bonus[KING + 1] = { 0, 0, 144, 144, 256, 517, 10000 };
+            m.value += (  150 * bool(threatByLesser[pt] & from)
+                        - 150 * bool(threatByLesser[pt] & to  )) * bonus[pt];
 
             if (ply < LOW_PLY_HISTORY_SIZE)
                 m.value += 8 * (*lowPlyHistory)[ply][m.from_to()] / (1 + ply);
