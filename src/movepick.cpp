44664f24a09cb5f0cef7e36cf667b317bddd2ab5
mstembera <m_stembera@yahoo.com>
2025-08-24 15:10:28 -0700

    Take 2 bench: 2411490

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -173,9 +173,8 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
             // penalty for moving to a square threatened by a lesser piece
             // or bonus for escaping an attack by a lesser piece.
             static constexpr int bonus[KING + 1] = {0, 0, 144, 144, 256, 517, 10000};
-            int v = threatByLesser[pt] & to ? -95 : 100 * bool(threatByLesser[pt] & from);
-            m.value += bonus[pt] * v;
-
+            m.value += (  100 * bool(threatByLesser[pt] & from)
+                        -  95 * bool(threatByLesser[pt] & to  )) * bonus[pt];
 
             if (ply < LOW_PLY_HISTORY_SIZE)
                 m.value += 8 * (*lowPlyHistory)[ply][m.from_to()] / (1 + ply);
