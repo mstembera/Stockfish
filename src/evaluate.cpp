47b64b2c4aaf90c5ad92a11c2f748372a4846832
mstembera <5421953+mstembera@users.noreply.github.com>
2025-11-18 18:35:35 -0800

    psqt and positional separately bench: 2186777

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -62,13 +62,13 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     auto [psqt, positional] = smallNet ? networks.small.evaluate(pos, accumulators, caches.small)
                                        : networks.big.evaluate(pos, accumulators, caches.big);
 
-    Value nnue = (125 * psqt + 131 * positional) * (64 + 5 * smallNet) / 8192;
+    Value nnue = ((250 - 13 * smallNet) * psqt + (262 - 27 * smallNet) * positional) / 256;
 
     // Re-evaluate the position when higher eval accuracy is worth the time spent
     if (smallNet && (std::abs(nnue) < 236))
     {
         std::tie(psqt, positional) = networks.big.evaluate(pos, accumulators, caches.big);
-        nnue                       = (125 * psqt + 131 * positional) / 128;
+        nnue                       = (250 * psqt + 262 * positional) / 256;
         smallNet                   = false;
     }
 
