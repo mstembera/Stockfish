174a98cd8d97490cacf76dc68dc40cf56a57ae4e
mstembera <5421953+mstembera@users.noreply.github.com>
2026-05-19 15:13:54 -0700

    Take 4 bench: 2394794

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -53,7 +53,7 @@ Value Eval::evaluate(const Eval::NNUE::Network&     network,
     assert(!pos.checkers());
 
     int sEv = simple_eval(pos);
-    if (std::abs(sEv) > 2 * RookValue)
+    if (std::abs(sEv) > RookValue + BishopValue)
         return sEv;
 
     auto [psqt, positional] = network.evaluate(pos, accumulators, caches);
