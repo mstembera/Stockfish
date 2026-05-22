411e1d8272627be287bc201202858843f786e987
mstembera <5421953+mstembera@users.noreply.github.com>
2026-05-22 12:25:45 -0700

    Take 6 w/ margin of error scaling bench: 2347231

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -53,8 +53,8 @@ Value Eval::evaluate(const Eval::NNUE::Network&     network,
     assert(!pos.checkers());
 
     int sEv = simple_eval(pos);
-    if (std::abs(sEv) >= RookValue + KnightValue + PawnValue)
-        return sEv;
+    if (std::abs(sEv) >= RookValue + BishopValue + PawnValue)
+        return sEv * 7 / 8;
 
     auto [psqt, positional] = network.evaluate(pos, accumulators, caches);
 
