bfe908534eb6dd4cd40da9cb7c6f59d91f4d1c51
mstembera <5421953+mstembera@users.noreply.github.com>
2026-07-18 14:53:12 -0700

    Take 3 bench: 2923853

--- src/search.cpp
+++ src/search.cpp
@@ -1266,8 +1266,8 @@ moves_loop:  // When in check, search starts here
                 if (!ss->inCheck && value > ss->staticEval)
                 {
                     const int bonus = std::clamp(
-                      int(value - ss->staticEval) * singularDepth * 200 / 1024,
-                      -CORRECTION_HISTORY_LIMIT / 4, CORRECTION_HISTORY_LIMIT / 4);
+                      int(value - ss->staticEval) * singularDepth * 230 / 1024,
+                      -CORRECTION_HISTORY_LIMIT / 3, CORRECTION_HISTORY_LIMIT / 3);
                     update_correction_history(pos, ss, *this, bonus);
                 }
 
