233ed9d6b262a3c053f029b8328d159b2c98a40b
mstembera <5421953+mstembera@users.noreply.github.com>
2026-07-19 19:17:31 -0700

    Early out from attackers_to_exist().  On top of PR6983 No functional change bench: 2792255

--- src/position.cpp
+++ src/position.cpp
@@ -652,10 +652,12 @@ Bitboard Position::attackers_to(Square s, Bitboard occupied) const {
 
 bool Position::attackers_to_exist(Square s, Bitboard occupied, Color c) const {
 
-    return (attacks_bb<ROOK>(s, occupied) & pieces(c, ROOK, QUEEN))
-        || (attacks_bb<BISHOP>(s, occupied) & pieces(c, BISHOP, QUEEN))
-        || (attacks_bb<PAWN>(s, ~c) & pieces(c, PAWN))
-        || (attacks_bb<KNIGHT>(s) & pieces(c, KNIGHT)) || (attacks_bb<KING>(s) & pieces(c, KING));
+    return (attacks_bb<AMAZON>(s) & pieces(c))
+        && (   (attacks_bb<ROOK>(s, occupied) & pieces(c, ROOK, QUEEN))
+            || (attacks_bb<BISHOP>(s, occupied) & pieces(c, BISHOP, QUEEN))
+            || (attacks_bb<PAWN>(s, ~c) & pieces(c, PAWN))
+            || (attacks_bb<KNIGHT>(s) & pieces(c, KNIGHT))
+            || (attacks_bb<KING>(s) & pieces(c, KING)));
 }
 
 // Tests whether a pseudo-legal move is legal
