3a7dd28b987cd0ffb037a7f49d6b66d18251753f
mstembera <5421953+mstembera@users.noreply.github.com>
2026-08-26 20:30:01 -0700

    Skip when the TT already predicts a fail low bench: 2664794

--- src/search.cpp
+++ src/search.cpp
@@ -1007,8 +1007,10 @@ Value Search::Worker::search(
     }
 
     // Step 10. Null move search with verification search
+    // Skip when the TT already predicts a fail low.
     if (cutNode && ss->staticEval >= beta - 13 * depth - 47 * improving + 365 && !excludedMove
-        && pos.non_pawn_material(us) && ss->ply >= nmpMinPly && beta >= -2000)
+        && pos.non_pawn_material(us) && ss->ply >= nmpMinPly && beta >= -2000
+        && !((ttData.bound & BOUND_UPPER) && is_valid(ttData.value) && ttData.value < beta))
     {
         assert((ss - 1)->currentMove != Move::null());
 
