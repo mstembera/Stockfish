e4dea7fcba100a16ac8d861a7d6a050737c17dc9
mstembera <5421953+mstembera@users.noreply.github.com>
2026-09-10 11:50:40 -0700

    Another tweak of both_attacks_bb() on top of PR7154 V3 No functional change bench: 2031952

--- src/attacks.h
+++ src/attacks.h
@@ -125,15 +125,15 @@ struct alignas(32) DualMagic {
         __m256i rev    = bswap(_mm256_sub_epi64(bswap(o), rrs));
         __m256i result = _mm256_and_si256(_mm256_xor_si256(fwd, rev), mask);
 
-        // Lane 0: bishop attacks; lane 1: unused
-        __m128i bishop =
-          _mm_or_si128(_mm256_extracti128_si256(result, 1), _mm_srli_si128(_mm256_castsi256_si128(result), 8));
+        // Lane 0: rook attacks (file only); lanes 1 and 2: bishop attacks
+        const __m256i rookBishop =
+          _mm256_or_si256(result, _mm256_permute4x64_epi64(result, _MM_SHUFFLE(3, 1, 2, 0)));
 
         Bitboard rowOccupancy = rankAttacksLookup[(occupied >> (shift + 1)) & 0x3f];
         Bitboard rankAttacks  = rowOccupancy << shift;
 
         // [bishop, rook]
-        return {_mm_cvtsi128_si64(bishop), _mm_cvtsi128_si64(_mm256_castsi256_si128(result)) + rankAttacks};
+        return {_mm_extract_epi64(_mm256_castsi256_si128(rookBishop), 1), _mm_cvtsi128_si64(_mm256_castsi256_si128(result)) + rankAttacks};
     }
 };
 
