595b345adb8252a22716059c280c9aed6ac9ae09
mstembera <5421953+mstembera@users.noreply.github.com>
2026-09-21 21:19:24 -0700

    Remove unnecessary variable from Position::see_ge() No functional change bench: 1687572

--- src/position.cpp
+++ src/position.cpp
@@ -1411,7 +1411,6 @@ bool Position::see_ge(Move m, int threshold) const {
     Color    stm       = sideToMove;
     Bitboard attackers = attackers_to(to, occupied);
     Bitboard stmAttackers, bb;
-    int      res = 1;
 
     while (true)
     {
@@ -1420,7 +1419,7 @@ bool Position::see_ge(Move m, int threshold) const {
 
         // If stm has no more attackers then give up: stm loses
         if (!(stmAttackers = attackers & pieces(stm)))
-            break;
+            return stm != sideToMove;
 
         // Don't allow pinned pieces to attack as long as there are
         // pinners on their original square.
@@ -1429,16 +1428,14 @@ bool Position::see_ge(Move m, int threshold) const {
             stmAttackers &= ~blockers_for_king(stm);
 
             if (!stmAttackers)
-                break;
+                return stm != sideToMove;
         }
-
-        res ^= 1;
 
         // Locate and remove the next least valuable attacker, and add to
         // the bitboard 'attackers' any X-ray attackers behind it.
         if ((bb = stmAttackers & pieces(PAWN)))
         {
-            if ((swap = PawnValue - swap) < res)
+            if ((swap = PawnValue + 1 - swap) <= 0)
                 break;
             occupied ^= least_significant_square_bb(bb);
 
@@ -1447,14 +1444,14 @@ bool Position::see_ge(Move m, int threshold) const {
 
         else if ((bb = stmAttackers & pieces(KNIGHT)))
         {
-            if ((swap = KnightValue - swap) < res)
+            if ((swap = KnightValue + 1 - swap) <= 0)
                 break;
             occupied ^= least_significant_square_bb(bb);
         }
 
         else if ((bb = stmAttackers & pieces(BISHOP)))
         {
-            if ((swap = BishopValue - swap) < res)
+            if ((swap = BishopValue + 1 - swap) <= 0)
                 break;
             occupied ^= least_significant_square_bb(bb);
 
@@ -1463,7 +1460,7 @@ bool Position::see_ge(Move m, int threshold) const {
 
         else if ((bb = stmAttackers & pieces(ROOK)))
         {
-            if ((swap = RookValue - swap) < res)
+            if ((swap = RookValue + 1 - swap) <= 0)
                 break;
             occupied ^= least_significant_square_bb(bb);
 
@@ -1472,9 +1469,9 @@ bool Position::see_ge(Move m, int threshold) const {
 
         else if ((bb = stmAttackers & pieces(QUEEN)))
         {
-            swap = QueenValue - swap;
+            swap = QueenValue + 1 - swap;
             //  implies that the previous recapture was done by a higher rated piece than a Queen (King is excluded)
-            assert(swap >= res);
+            assert(swap > 0);
             occupied ^= least_significant_square_bb(bb);
 
             const auto [bishopAttacks, rookAttacks] = both_attacks_bb(to, occupied);
@@ -1485,10 +1482,10 @@ bool Position::see_ge(Move m, int threshold) const {
         else  // KING
               // If we "capture" with the king but the opponent still has attackers,
               // reverse the result.
-            return (attackers & ~pieces(stm)) ? res ^ 1 : res;
+            return (attackers & ~pieces(stm)) ? stm != sideToMove : stm == sideToMove;
     }
 
-    return bool(res);
+    return stm == sideToMove;
 }
 
 // Tests whether the position is drawn by 50-move rule
